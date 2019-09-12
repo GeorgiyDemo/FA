36fe57ce6de693051f6099ae5796ff1985f5dcab
Georgiy Demo <demenchuk.george@protonmail.com>
2019-09-12 21:26:33 +0300

    Завершение работы с заданиями на строки

--- Практика Python/pract2/task3.py
+++ Практика Python/pract2/task3.py
@@ -10,6 +10,21 @@ dictionaries = {
     1 : "abc0123456789ldefghijkmnopqrstuvwxyz"
 }
 
+def check_values(check_str):
+        
+    """
+    Метод для возврата значения boolen того, 
+    чтоб символы кодируемого слова содержались в алфавитах  
+    """
+
+    bool_flag = True
+    for char in check_str:
+        for d in dictionaries:
+            if char not in dictionaries[d]:
+                bool_flag = False
+    
+    return bool_flag
+
 class PolyWordClass(object):
     
     def __init__(self, s, key):
@@ -20,15 +35,14 @@ class PolyWordClass(object):
         #Шифруем слово
         self.main_encoded()
 
-        #TODO Расшифровка слова
+        #Расшифровка слова
         self.main_decoded()
         
 
-    
-
     def char_encoded(self, d, char):
         """
-        Метод для шифрования данных по шифру Цезаря
+        Метод для шифрования данных по классическому шифру Цезаря
+        (взял со своего задания №1)
         """
         k = self.key
         index = d.find(char)
@@ -42,13 +56,31 @@ class PolyWordClass(object):
         else:
             return d[index+k]
 
+    def char_decoded(self, d, char):
+        """
+        Метод для расшифровки данных по классическому шифру Цезаря
+        (взял со своего задания №1)
+        """
+        k = self.key
+        index = d.find(char)
+
+        #Если длина ключа больше самой строки
+        if k > len(d):
+            k = k-len(d)
+
+        if index-k >= len(d):
+            return d[index-k+len(d)]
+        else:
+            return d[index-k]
+
     def main_encoded(self):
         
         """
-        Метод для шифрования данных c использованием кодового слова
-        
+        Метод для определения того, какой словарь необходимо использовать
+        В данном случае идёт чередование по вызову dict dictionaries (масло масленное) 
+        в зависмости от чётности переменной цикла i
         """
-        #Исходная строка
+
         input_str = self.s
         out = ""
         for i in range(len(input_str)):
@@ -56,10 +88,21 @@ class PolyWordClass(object):
         self.encoded = out
 
     def main_decoded(self):
-        self.decoded = "TEST"
+
+        """
+        Метод аналогичен main_encoded, но только с 
+        вызовом self.char_decoded вместо self.char_encoded
+        """
+
+        input_str = self.encoded
+        out = ""
+        for i in range(len(input_str)):
+            out += self.char_decoded(dictionaries[i % 2], input_str[i])
+        self.decoded = out
 
 def main():
     try:
+
         k = int(input("Введите сдвиг -> "))
         s = str(input("Введите строку -> "))
         
@@ -67,13 +110,14 @@ def main():
         print("Что-то пошло не так при вводе данных")
         return
     
+    if check_values(s) == False:
+        print("Нет символов введенной строки в исходном алфавите!")
+        return
+    
     obj = PolyWordClass(s, k)
     
-    print("\n**Шифрование**")
-    print("\nРезультат:\n"+obj.encoded)
-
-    print("\n**Расшифровка**")
-    print("\nРезультат:\n"+obj.decoded)
+    print("\n**Шифрование**\nРезультат: " + obj.encoded)
+    print("\n**Расшифровка**\nРезультат: " + obj.decoded)
 
 if __name__ == "__main__":
     main()
