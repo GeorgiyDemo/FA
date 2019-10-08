82358f82e63f44052b868dd2accdefb7a9df9822
Georgiy Demo <demenchuk.george@protonmail.com>
2019-10-08 12:12:08 +0300

    Двусвязные списки

--- Алгоритмы Python/семинары/pract3/hard_tasks.py
+++ Алгоритмы Python/семинары/pract3/hard_tasks.py
@@ -1,5 +1,5 @@
 """
-Разбор односвязанных списков по заданиям 13-
+Разбор двусвязных списков по заданиям 13-14
 """
 
 def print_backward(lst):
@@ -8,42 +8,42 @@ def print_backward(lst):
     head = lst
     tail = lst.next
     print_backward(tail)
-    print(head)
-
-def print_list(node):
-    while node:
-        print(node)
-        node = node.next
-    print
+    print(head, head.previous)
 
 class Node:
-    def __init__(self, cargo=None, next=None):
-        self.cargo = cargo
+    def __init__(self, value=None, next=None, previous=None):
+        self.value = value
         self.next  = next
+        self.previous = previous
 
     def __str__(self):
-        return str(self.cargo)
+        return str(self.value)
 
-node1 = Node(1)
-node2 = Node(2)
-node1.next = node2
-node3 = Node(3)
-node2.next = node3
-node4 = Node(4)
-node3.next = node4
+def main():
+    node1 = Node(1)
+    node2 = Node(2)
+    node1.next = node2
+    node2.previous = node1
 
-print("Узел 1")
-print_list(node1)
-print_backward(node1)
+    node3 = Node(3)
+    node2.next = node3
+    node3.previous = node2
 
-print("Узел 2")
-print_list(node2)
-print_backward(node2)
+    node4 = Node(4)
+    node3.next = node4
+    node4.previous = node3
 
-print("Узел 3")
-print_list(node3)
-print_backward(node3)
+    print("Узел 1")
+    print_backward(node1)
 
-print("Узел 4")
-print_list(node4)
-print_backward(node4)
+    print("Узел 2")
+    print_backward(node2)
+
+    print("Узел 3")
+    print_backward(node3)
+
+    print("Узел 4")
+    print_backward(node4)
+
+if __name__ == "__main__":
+    main()
