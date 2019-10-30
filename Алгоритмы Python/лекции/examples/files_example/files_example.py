0ddeb2cb501817b60f8e34478f7ca9e85424e321
Georgiy Demo <demenchuk.george@protonmail.com>
2019-10-30 23:58:14 +0300

    Материал по файлам по Python
    Набрал 0 из 2 на тестировании, да, да, да

---
+++ Алгоритмы Python/лекции/examples/files_example/files_example.py
@@ -0,0 +1,21 @@
+# бинарный режим:
+with open("new_file6.txt", "wb") as f: 
+    f.write(bytes("Строка1\nСтрока2", "utf-8")) 
+    f.write(bytearray("\nCтpoкaЗ", "utf-8")) 
+
+with open("new_file6.txt", "r") as f: 
+    print(f.read())
+
+import pickle # подключаем модуль pickle
+import json
+import yaml
+
+check_elements = {}
+for i in range(6):
+    check_elements[i] = i
+
+with open('./obj.pickle', 'wb') as f:
+    pickle.dump(check_elements, f)
+
+with open("./obj.yml", 'w') as stream:
+    yaml.safe_dump(check_elements,stream)
