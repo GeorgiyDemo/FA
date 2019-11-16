79a9eb2226035443011b7c101eafbb7eb596ab12
Georgiy Demo <demenchuk.george@protonmail.com>
2019-11-16 17:56:27 +0300

    Рефраз + относительно красивый UI для выбора вагона #10
    Теперь необходим UI для выбора мест в конкретном вагоне + чекай TODO

--- Алгоритмы Python/семинары/pract5/task3_6/ticket_module.py
+++ Алгоритмы Python/семинары/pract5/task3_6/ticket_module.py
@@ -7,24 +7,29 @@ import yaml
 
 def check_reservers_by_name(d, name):
     """
-    Метод полезен тажке для отмены бронирования
+    Метод находит по ФИО брони все билеты в коллекции
+    
+    Также полезен для отмены бронирования #TODO
     """
-    trains_with_name = []
-    for way in d:
-        for train in way["train"]:
-            for place in way["train"][train]["cars"]:
-                current_place_dict = way["train"][train]["cars"][place]
+    trains_str = []
+    trains = []
+    for i in range(len(d)):
+        for car in d[i]["train"]:
+            for place in d[i]["train"][car]["cars"]:
+                current_place_dict = d[i]["train"][car]["cars"][place]
                 if current_place_dict["name"] == name:
                     price_str = str(current_place_dict["price"])
-                    print("["+price_str+"руб.] Поезд по пути '"+way["from"]+"' -> '"+way["to"]+"' Вагон №"+train+", место "+place)
+                    trains_str.append("["+price_str+" руб.] Поезд по пути '"+d[i]["from"]+"' -> '"+d[i]["to"]+"' Вагон №"+car+", место "+place)
+                    trains.append([i,car,place])
                 
-                #for place in way["train"][train]["cars"][car]:
-
-                #    locale_dict = way["train"][train]["cars"][car]
-                #    print(locale_dict[place])
-    return ""
+    return (trains_str, trains)
     
 class FileClass():
+    """
+    Класс для работы с файлом file_name, который передаётся в конструктр из main
+    - Записывает данные в файл
+    - Читает данные из файла 
+    """
 
     def __init__(self, file_name, method):
         self.file_name = file_name
@@ -50,12 +55,26 @@ class FileClass():
 
 
 class AddTicketClass():
-    def __init__(self, file_name):
+    """
+    Класс для добавления билетов. Выщывается как при прямых рейсов, так и для ресов с пересадками
+    #TODO В конструкторе ввести поля для автобронирования мест при использовании пункта 1
+    """
+    def __init__(self, file_name, way_from=None, way_to=None):
+        
+        self.selected_car = None
+        self.file_name = file_name 
+        self.way_from = way_from
+        self.way_to = way_to
+
+        print("Загрузка..")
         obj = FileClass(file_name,2)
         self.content = obj.get_text()
-        self.file_name = file_name 
+        
         self.add_reserve()
-
+        self.car_searcher()
+        if self.selected_car != None:
+            self.place_searcher()
+        
     def add_reserve(self):
         """
         Файл добавления данных в файл
@@ -64,16 +83,45 @@ class AddTicketClass():
         """
         new_name = input("Введите ФИО пассажира -> ")
         check_name_tuple = check_reservers_by_name(self.content,new_name)
-        if check_name_tuple != "":
+        if check_name_tuple[0] != []:
             print("Ваши брони:")
-            print(check_name_tuple)
-        #print("Весь словарь:")
-        #for k in self.content:
-        #    print(k)
-
-        #    print("-")
-        #car_places_dict[str(i)] = {"name":"Кот занял место", "price":1300, "type":"reserved"}
+            print('\n'.join(check_name_tuple[0]))
+        else:
+            print("Броней, связанных с введенными ФИО не найдено")
+        
+        if self.way_from == None and self.way_to == None:
+            self.way_from = input("Введите странцию отправления -> ")
+            self.way_to = input("Введите станцию прибытия -> ")
     
+    def car_searcher(self):
+        """
+        Выбор вагона для брони в поезде
+        #TODO Проверка, если нет таких поездов совсем + выбор поезда на пути
+        #TODO Также можно добавить диапазон цен на места
+        """
+        content = self.content
+        for train in content:
+            if train["from"] == self.way_from and train["to"] == self.way_to:
+                print("Поезд найден "+train["from"]+" -> "+train["to"])
+                print("Всего в поезде "+str(train["info"]["car_count"])+" вагонов и "+str(train["info"]["places_free"])+" свободных мест")
+                print('\nМест по вагонам:\n{0:10}  {1}'.format("Вагон", "Мест свободно"))
+                buf_car_list = []
+
+                for car in train["train"]:
+                    print('{0:10}  {1}'.format(car, train["train"][car]["places_free"]))
+                    buf_car_list.append(car)
+                
+                selected_car = input("Выберите вагон -> ")
+                if selected_car in buf_car_list:
+                    self.selected_car = selected_car
+                else:
+                    print("Нет такого вагона, выход из подпрограммы..")
+    
+    def place_searcher(self):
+        """
+        Выбор места в вагоне для брони
+        """
+        pass
 
 class RemoveTicketClass():
     """
@@ -82,17 +130,28 @@ class RemoveTicketClass():
     def __init__(self, file_name):
         self.remove_reserve()
 
+    def ticket_remover(self):
+        #делает None на место + обращается к FileClass для записи обновлённого файла
+        pass
+
     def remove_reserve(self):
         """
         Файл добавления данных в файл
         - Спрашивает номер вагона 
         - Спрашивает номер места
         """
+        new_name = input("Введите ФИО пассажира -> ")
+        check_name_tuple = check_reservers_by_name(self.content,new_name)
+        if check_name_tuple[0] != []:
+            print("Ваши брони:")
+            print('\n'.join(check_name_tuple[0]))
+            remove_ticket = input("Какую бронь хотите отменить?\n-> ")
+
+        else:
+            print("Броней, связанных с введенными ФИО не найдено")
         #car_places_dict[str(i)] = {"name":"Кот занял место", "price":1300, "type":"reserved"}
         pass
 
-    def remove_reserve(self):
-        pass
 
 ###Временно
 if __name__ == "__main__":
