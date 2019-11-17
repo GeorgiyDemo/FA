ee15d86b1f8bd254d9e4ab8667895b5f256e189e
Georgiy Demo <demenchuk.george@protonmail.com>
2019-11-18 00:30:19 +0300

    Удалили выполненные TODO, осталось только реформат кода и всё

--- Алгоритмы Python/семинары/pract5/task3_6/ticket_module.py
+++ Алгоритмы Python/семинары/pract5/task3_6/ticket_module.py
@@ -98,106 +98,119 @@ class AddTicketClass():
     def mechanical_components_reserve(self):
         """
         Выбор вагона для брони в поезде
-        #TODO Проверка, если нет таких поездов совсем + выбор поезда на пути
         """
+        exit_flag = False
+        while exit_flag == False:
+            content = self.content
 
-        content = self.content
+            for i in range(len(content)):
+                if content[i]["from"] == self.way_from and content[i]["to"] == self.way_to:
+                    print("Поезд найден "+content[i]["from"]+" -> "+content[i]["to"])
+                    print("Всего в поезде "+str(content[i]["info"]["car_count"])+" вагонов и "+str(content[i]["info"]["places_free"])+" свободных мест\nМест по вагонам:")
+                    table = texttable.Texttable(180)
+                    table_list = [["Вагон", "Мест свободно", "Диапазон цен на места"],]
 
-        for i in range(len(content)):
-            if content[i]["from"] == self.way_from and content[i]["to"] == self.way_to:
-                print("Поезд найден "+content[i]["from"]+" -> "+content[i]["to"])
-                print("Всего в поезде "+str(content[i]["info"]["car_count"])+" вагонов и "+str(content[i]["info"]["places_free"])+" свободных мест\nМест по вагонам:")
-                table = texttable.Texttable(180)
-                table_list = [["Вагон", "Мест свободно", "Диапазон цен на места"],]
+                    buf_car_list = []
 
-                buf_car_list = []
+                    for car in content[i]["train"]:
+                        price_min, price_max = get_min_max_price_of_car(content[i]["train"][car])
+                        price_range = str(price_max) + " - "+ str(price_min)+" руб."
+                        table_list.append([car, content[i]["train"][car]["places_free"],price_range])
+                        buf_car_list.append(car)
+                    
+                    #Вывод
+                    table.add_rows(table_list)
+                    print(table.draw() + "\n")
 
-                for car in content[i]["train"]:
-                    price_min, price_max = get_min_max_price_of_car(content[i]["train"][car])
-                    price_range = str(price_max) + " - "+ str(price_min)+" руб."
-                    table_list.append([car, content[i]["train"][car]["places_free"],price_range])
-                    buf_car_list.append(car)
-                
-                #Вывод
-                table.add_rows(table_list)
-                print(table.draw() + "\n")
-
-                selected_car = input("Выберите вагон -> ")
-                if selected_car in buf_car_list:
-                    self.place_searcher(i,selected_car)
-                else:
-                    print("Нет такого вагона, выход из подпрограммы..")
-        self.content = content
+                    selected_car = input("Выберите вагон (0 для выхода из подменю) -> ")
+                    if selected_car == "0":
+                        exit_flag = True 
+                    elif selected_car in buf_car_list:
+                        self.place_searcher(i,selected_car)
+                    else:
+                        print("Нет такого вагона, выход из подпрограммы..")
+                        exit_flag = True
+            self.content = content
         
     def place_searcher(self, way, selected_car):
         """
         Выбор места в вагоне для брони
         """
-        content = self.content
-        table = texttable.Texttable(180)
-        print("Места в вагоне:")
-        table_list = [["№", "Статус", "Цена", "Тип"],]
-        buf_place_list = []
-        for place in content[way]["train"][selected_car]["cars"]:
-            buf_place_list.append(place)
-            locale_place = content[way]["train"][selected_car]["cars"][place]
-            
-            reserved_type = "свободно"
-            if locale_place["name"] != None:
-                reserved_type = "забронировано"
-            price = str(locale_place["price"])+" руб."
-            table_list.append([place, reserved_type, price, locale_place["type"]])
-        
-        table.add_rows(table_list)
-        print(table.draw() + "\n")
-        
-        selected_place = input("Введите номер места для бронирования -> ")
-        if selected_place in buf_place_list:
-            question_string = "Вы действительно хотите забронировать место №"+selected_place+" в вагоне "+selected_car+" поезда "+self.way_from+" - "+self.way_to+" на имя '"+self.name+"'? (Да/Нет) -> "
-            user_reply = input(question_string)
-            if user_reply == "Да" or user_reply == "Y" or user_reply == "y":
+        exit_flag = False
 
-                #Успешно меняем текущий словарь
-                content[way]["train"][selected_car]["cars"][selected_place]["name"] = self.name
-                question_input = input("Место успешно зарезервировано\nОплатить его сейчас? Да/Нет -> ")
-                if question_input == "Да":
-                    pay_obj = PaymentClass()
-                    if pay_obj.result == True:
-                        #Устанавливаем флаг того, что мы всё оплатили
-                        content[way]["train"][selected_car]["cars"][selected_place]["payment"] = 1
-                        
-                        #Формируем квитанцию об оплате
-                        date_now = datetime.datetime.now().strftime("%H.%M.%S %d:%m:%Y")
-                        report_filename = "Квитанция об оплате "+self.name+" от "+date_now+".pdf"
-                        header_str = "Квитанция об оплате заказа от\n"+date_now
-                        main_text_str_name = "ФИО клиента: "+self.name
-                        
-                        buf_place = content[way]["train"][selected_car]["cars"][selected_place]
-
-                        main_text_str_ticket = "\nБилет\nСтанция и время отправления: "+content[way]["from"]+" "+content[way]["time_begin"]+"\n"+ "Место и время прибытия: "+content[way]["to"]+" "+content[way]["time_finish"]+"\nМесто: №"+selected_place+" вагон "+selected_car+" ["+buf_place["type"]+"]"
-                        main_text_str_payment = "\nОплата\nСтоимость билета: "+ str(buf_place["price"])+" руб. \n"+"Статус оплаты: ОПЛАЧЕНО"
-                        main_text_str = main_text_str_name+"\n"+main_text_str_ticket + "\n" + main_text_str_payment
-                        qr_text = main_text_str
-                        PDF_obj = file_writer_module.PDFWriter(header_str, main_text_str,qr_text, report_filename)
-                        if PDF_obj.processed_flag == True:
-                            print("Квитанция об оплате успешно сформирована")
-
-                elif question_input == "Нет":
-                    print("Хорошо, оплатить билет вы можете позже в пункте 2 'Управление моими билетами'")
+        while exit_flag == False:
+            content = self.content
+            table = texttable.Texttable(180)
+            print("Места в вагоне:")
+            table_list = [["№", "Статус", "Цена", "Тип"],]
+            buf_place_list = []
+            for place in content[way]["train"][selected_car]["cars"]:
+                buf_place_list.append(place)
+                locale_place = content[way]["train"][selected_car]["cars"][place]
                 
-                #Обновляем количество мест
-                train_places_free = content[way]["info"]["places_free"] - 1
-                car_places_free = content[way]["train"][selected_car]["places_free"] - 1 
+                reserved_type = "свободно"
+                if locale_place["name"] != None:
+                    reserved_type = "забронировано"
+                price = str(locale_place["price"])+" руб."
+                table_list.append([place, reserved_type, price, locale_place["type"]])
+            
+            table.add_rows(table_list)
+            print(table.draw() + "\n")
+            
+            selected_place = input("Введите номер места для бронирования (0 для выхода из подменю) -> ")
+            if selected_place == "0":
+                exit_flag = True
+            elif selected_place in buf_place_list:
+                if content[way]["train"][selected_car]["cars"][selected_place]["name"] == None:
+                    question_string = "Вы действительно хотите забронировать место №"+selected_place+" в вагоне "+selected_car+" поезда "+self.way_from+" - "+self.way_to+" на имя '"+self.name+"'? (Да/Нет) -> "
+                    user_reply = input(question_string)
+                    if user_reply == "Да" or user_reply == "Y" or user_reply == "y":
 
-                content[way]["info"]["places_free"] = train_places_free
-                content[way]["train"][selected_car]["places_free"] = car_places_free
+                        #Успешно меняем текущий словарь
+                        content[way]["train"][selected_car]["cars"][selected_place]["name"] = self.name
+                        question_input = input("Место успешно зарезервировано\nОплатить его сейчас? Да/Нет -> ")
+                        if question_input == "Да":
+                            pay_obj = PaymentClass()
+                            if pay_obj.result == True:
+                                #Устанавливаем флаг того, что мы всё оплатили
+                                content[way]["train"][selected_car]["cars"][selected_place]["payment"] = 1
+                                
+                                #Формируем квитанцию об оплате
+                                date_now = datetime.datetime.now().strftime("%H.%M.%S %d:%m:%Y")
+                                report_filename = "Квитанция об оплате "+self.name+" от "+date_now+".pdf"
+                                header_str = "Квитанция об оплате заказа от\n"+date_now
+                                main_text_str_name = "ФИО клиента: "+self.name
+                                
+                                buf_place = content[way]["train"][selected_car]["cars"][selected_place]
 
-                #Записываем все в файл
-                writer_obj = universal_module.FileClass(self.file_name)
-                writer_obj.set_file(content)
-        else:
-            print("Введенное место не найдено, выход из подпрограммы..")
-        self.content = content
+                                main_text_str_ticket = "\nБилет\nСтанция и время отправления: "+content[way]["from"]+" "+content[way]["time_begin"]+"\n"+ "Место и время прибытия: "+content[way]["to"]+" "+content[way]["time_finish"]+"\nМесто: №"+selected_place+" вагон "+selected_car+" ["+buf_place["type"]+"]"
+                                main_text_str_payment = "\nОплата\nСтоимость билета: "+ str(buf_place["price"])+" руб. \n"+"Статус оплаты: ОПЛАЧЕНО"
+                                main_text_str = main_text_str_name+"\n"+main_text_str_ticket + "\n" + main_text_str_payment
+                                qr_text = main_text_str
+                                PDF_obj = file_writer_module.PDFWriter(header_str, main_text_str,qr_text, report_filename)
+                                if PDF_obj.processed_flag == True:
+                                    print("Квитанция об оплате успешно сформирована")
+
+                        elif question_input == "Нет":
+                            print("Хорошо, оплатить билет вы можете позже в пункте 'Управление моими билетами'")
+                        
+                        #Обновляем количество мест
+                        train_places_free = content[way]["info"]["places_free"] - 1
+                        car_places_free = content[way]["train"][selected_car]["places_free"] - 1 
+
+                        content[way]["info"]["places_free"] = train_places_free
+                        content[way]["train"][selected_car]["places_free"] = car_places_free
+
+                        #Записываем все в файл
+                        writer_obj = universal_module.FileClass(self.file_name)
+                        writer_obj.set_file(content)
+
+                else:
+                    print("Введенное место уже забронировано")
+            else:
+                print("Введенное место не найдено, выход из подпрограммы..")
+                exit_flag = True
+            self.content = content
 
 class RemoveTicketClass():
 
