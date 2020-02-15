861407a3fb92bf32c45e0f86bc396f81daeb889d
Georgiy Demo <demenchuk.george@protonmail.com>
2020-02-15 11:28:22 +0300

    4 #20

--- Алгоритмы Python/Part2/семинары/pract1/begining/task1-4.py
+++ Алгоритмы Python/Part2/семинары/pract1/begining/task1-4.py
@@ -1,3 +1,5 @@
+import math
+
 class Task1Class:
     """
     Задача 1. Создайте класс с методом класса, в котором определялась бы сумма двух целых чисел.
@@ -33,11 +35,50 @@ class Task4Class:
 значения функции г и вывод на экран результатов вычислений, Исходные данные в соответстви
 с вариантом функции первого семестра.
     """
-    pass
 
+    def __init__(self, x):
+        self.x = x
+        self.getter()
 
-if __name__ == "__main__":
+    def getter(self):
 
+        x = self.x
+        upper = x ** 3 * math.e ** (x - 1)
+        lower = x ** 3 - math.fabs(x)
+        if lower == 0:
+            print("Знаменатель равен нулю, деление на 0!")
+            self.result = 0
+            return
+
+        first = upper / lower
+
+        log_sqrt = math.sqrt(x) - x
+
+        if log_sqrt >= 0:
+            buf_log = math.log(log_sqrt, 2)
+        else:
+            print("Выражение в log[sqrt(x)-x,2] меньше 0!")
+            self.result = 0
+            return
+
+        self.result = first - buf_log
+
+def main():
     print("Задание 1:", Task1Class.sum(3,4))
     obj = Task2Class(2,4)
     print("Задание 2:", obj.sum())
+
+
+    print("*Задание 4 на условные операторы*")
+    try:
+        x = float(input("Введите x -> "))
+    except:
+        print("Проблема ввода данных!")
+        return
+    
+    obj = Task4Class(x)
+    print("Результат: " + str(obj.result))
+
+
+if __name__ == "__main__":
+    main()
