faf137b37cb97f6b5232d41858aca83a231c9c9a
Georgiy Demo <demenchuk.george@protonmail.com>
2020-02-18 17:46:24 +0300

    Часть 7, бейкап на 2% #20

--- Алгоритмы Python/Part2/семинары/pract1/second/task7.py
+++ Алгоритмы Python/Part2/семинары/pract1/second/task7.py
@@ -6,8 +6,66 @@
 Создайте дочерние классы ПЕРСОНА (фамилия, адрес, номер телефона),
 ОРГАНИЗАЦИЯ (название, адрес, телефон, факс, контактное лицо),
 ДРУГ (фамилия, адрес, номер телефона, дата рождения)
+
 со своими методами вывода информации на экран и определения соответствия заданной фамилии.
 
 Создайте список из п записей, выведите полную информацию из базы на экран,
  а также организуйте поиск в базе по фамилии.
 """
+
+class PhoneDictionaryClass:
+    """
+    Класс телефонный справочник
+    """
+
+    def search(self, input_name):
+        if input_name in self.name:
+            return True
+        return False
+
+    def out_info(self):
+        pass
+
+
+class PersonClass(PhoneDictionaryClass):
+    """
+    Класс персона
+    """
+
+    def __init__(self, name, address, phone_number):
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+
+    def out_info(self):
+        pass
+
+class OrganizationClass(PhoneDictionaryClass):
+    """
+    Класс организация
+    """
+
+    def __init__(self, name, address, phone_number, fax, contact_person):
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+        self.fax = fax
+        self.contact_person = contact_person
+
+
+class FriendClass(PhoneDictionaryClass):
+    """
+    Класс друг
+    """
+
+    def __init__(self, name, address, phone_number, birth_date):
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+        self.birth_date = birth_date
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
