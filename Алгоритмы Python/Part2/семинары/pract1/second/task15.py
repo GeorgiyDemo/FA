09c622aa84ee85983820bff2340eb1979019bcfc
Georgiy Demo <demenchuk.george@protonmail.com>
2020-02-25 22:23:23 +0300

    Готово 15 #20

--- Алгоритмы Python/Part2/семинары/pract1/second/task15.py
+++ Алгоритмы Python/Part2/семинары/pract1/second/task15.py
@@ -5,12 +5,13 @@
 Создайте список п прогрессий и выведите сумму каждой из них экран.
 """
 
-class ProgressionClass:
-    def __init__(self, a1, a2, n):
-        self.a1 = a1
-        self.a2 = a2
-        self.n = n        
+from random import randint
+
+class ProgressionClass:    
     
+    def get_sum(self):
+        return self.s
+
     def calculate(self):
         ...
 
@@ -19,33 +20,59 @@ class ProgressionClass:
 
 class ArithmeticClass(ProgressionClass):
     def __init__(self, a1, a2, n):
-        super().__init__(a1, a2, n)
+        self.a1 = a1
+        self.a2 = a2
+        self.n = n
+
         self.calculate()
 
     def calculate(self):
         self.s = (self.a1 + self.a2)*self.n/2
 
     def info(self):
-        return "[Арифметическая прогрессия]\n1-й элемент: "+str(self.a1)+"\n2-й элемент: "+str(self.a2)+"\nШаг: "+str(self.n)+"Сумма:"+str(self.s)
+        return "[Арифметическая прогрессия]\n1-й элемент: "+str(self.a1)+"\n2-й элемент: "+str(self.a2)+"\nШаг: "+str(self.n)+"\nСумма: "+str(self.s)
 
 class GeometricClass(ProgressionClass):
     def __init__(self, b1, q):
         self.b1 = b1
         self.q = q
-        self.geometric()
+        self.calculate()
 
     def calculate(self):
-        self.S = self.b1 / (1 - self.q)
-        return self.S
+        self.s = self.b1 / (1 - self.q)
 
     def info(self):
-        return "[Геометрическая прогрессия]\n1-й элемент: "+str(self.a1)+"\n2-й элемент: "+str(self.a2)+"\nШаг: "+str(self.n)+"Сумма:"+str(self.s)
+        return "[Геометрическая прогрессия]\nЗнаменатель прогрессии: "+str(self.q)+"\n1-й элемент: "+str(self.b1)+"\nСумма: "+str(self.s)
 
-
-Arithmetic(6, 25.5, 40).info()
-Geometric(27, 1/3).info()
 
 def main():
-    pass
+    #Создайте список п прогрессий и выведите сумму каждой из них экран.
+    try:
+        n = int(input("Введите кол-во прогрессий для генерации -> "))
+    except ValueError:
+        print("Некорректный ввод данных")
+        return
+        
+    d = {
+        1 : ArithmeticClass,
+        2 : GeometricClass,
+    }
+    
+    progression_list = []
+
+    for _ in range(n):
+        
+        d_args = {
+            1 : [randint(-1000,1000),randint(-1000,1000),randint(1,10)],
+            2 : [randint(-1000,1000),randint(-1000,1000)],
+        }
+
+        r_number = randint(1,2)
+        progression_list.append(d[r_number](*d_args[r_number]))
+    
+    for e in progression_list:
+        #print(e.get_sum())
+        print(e.info()+"\n")
+    
 if __name__ == "__main__":
     main()
