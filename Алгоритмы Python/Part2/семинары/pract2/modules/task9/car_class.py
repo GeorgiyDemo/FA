ab2415e0beac18b05be9cc2f38d5a67507ddf661
Georgiy Demo <demenchuk.george@protonmail.com>
2020-03-18 00:58:32 +0300

    Задание 9

---
+++ Алгоритмы Python/Part2/семинары/pract2/modules/task9/car_class.py
@@ -0,0 +1,7 @@
+from transport_class import TransportClass
+class CarClass(TransportClass):
+    def __init__(self, model, number, speed, carrying):
+        super().__init__(model, number, speed, carrying)
+    def get_info(self):
+        return "[Автомобиль]\nМарка: " + self.model + "\nНомер: " + self.number + "\nСкорость: " + str(
+            self.speed) + "\nГрузоподъёмность: " + str(self.carrying)
