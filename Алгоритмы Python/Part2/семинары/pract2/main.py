c713258e3abfde123a67f6caab89f6499280cca2
Georgiy Demo <demenchuk.george@protonmail.com>
2020-03-17 14:53:45 +0300

    Везде в модулях прикручиваем main в __init__.py #29

--- Алгоритмы Python/Part2/семинары/pract2/main.py
+++ Алгоритмы Python/Part2/семинары/pract2/main.py
@@ -1,5 +1,10 @@
+from modules import task5, task4, task3, task2, task1
 
 def main():
+    d_selector = {"1" : task1, "2" : task2, "3" : task3, "4" : task4, "5" : task5}
+    input_str = input("Введите номер задания (1-20) ->")
+    if input_str in d_selector: d_selector[input_str].main()
+    '''
     print("Задание 1:", Task1Class.sum(3, 4))
     obj = Task2Class(2, 4)
     print("Задание 2:", obj.sum())
@@ -16,7 +21,7 @@ def main():
 
     obj = Task4Class(x)
     print("Результат: " + str(obj.result))
-
+'''
 
 if __name__ == "__main__":
     main()
