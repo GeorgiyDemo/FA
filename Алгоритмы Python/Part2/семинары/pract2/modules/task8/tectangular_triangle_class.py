e61aa52be243503c5b006d4e9dc8205383140b70
Georgiy Demo <demenchuk.george@protonmail.com>
2020-03-17 22:55:55 +0300

    Задание 8 #29

---
+++ Алгоритмы Python/Part2/семинары/pract2/modules/task8/tectangular_triangle_class.py
@@ -0,0 +1,18 @@
+import math
+from triangle_class import TriangleClass
+class TectangularTriangleClass(TriangleClass):
+    def __init__(self, a_side, b_side, angle=90):
+        self.angle = 90
+        super().__init__(a_side, b_side, angle)
+        self.a_side = a_side
+        self.b_side = b_side
+        self.perimeter_calculation()
+        self.area_calculation()
+        self.area_calculation()
+    def area_calculation(self):
+        self.area = (1 / 2) * self.a_side * self.b_side
+    def perimeter_calculation(self):
+        a = self.a_side
+        b = self.b_side
+        c = math.sqrt(pow(a, 2) + pow(b, 2))
+        self.perimeter = a + b + c
