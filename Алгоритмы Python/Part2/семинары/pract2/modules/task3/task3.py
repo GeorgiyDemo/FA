dd19fb0fca45480c2a54aaba4eb2d2c26842b93f
Georgiy Demo <demenchuk.george@protonmail.com>
2020-03-18 13:48:49 +0300

    Сокращение 3 до 25 строк

--- Алгоритмы Python/Part2/семинары/pract2/modules/task3/task3.py
+++ Алгоритмы Python/Part2/семинары/pract2/modules/task3/task3.py
@@ -2,25 +2,18 @@ from random import randrange
 class Task3Class:
     def __init__(self):
         try:
-            d = {
-                "1": self.user_input,
-                "2": self.random_input,
-            }
+            d = {"1": self.user_input,"2": self.random_input,}
             self.main_list = []
             self.list_len = int(input("Введите длинну последовательности -> "))
             method_number = input("Как вы хотите заполнить последовательность?\n1. Вручную\n2. Автоматически\n-> ")
             if method_number in d:
                 d[method_number]()
                 self.out_list()
-            else:
-                print("Некорректный ввод")
-        except Exception as e:
-            print("Возникла ошибка: ", e)
+            else: print("Некорректный ввод")
+        except Exception as e: print("Возникла ошибка: ", e)
     def number_checker(self, e):
-        try:
-            return int(e)
-        except ValueError:
-            return e
+        try: return int(e)
+        except ValueError: return e
     def user_input(self):
         for i in range(self.list_len):
             locale_element = self.number_checker(input("Введите элемент №" + str(i + 1) + " -> "))
