bc9dcdcc6d645e68fbd7d1c1b725b361c60a0861
Georgiy Demo <demenchuk.george@protonmail.com>
2020-03-18 12:12:29 +0300

    16 - 17  #29

---
+++ Алгоритмы Python/Part2/семинары/pract2/modules/task17/ball_class.py
@@ -0,0 +1,11 @@
+from body_class import BodyClass
+from math import pi
+class BallClass(BodyClass):
+    def __init__(self, r):
+        self.r = r
+        self.volume_calculation()
+        self.surface_area_calculation()
+    def surface_area_calculation(self):
+        self.surface_area = 4 * pi * pow(self.r, 2)
+    def volume_calculation(self):
+        self.volume = (4 / 3) * pi * pow(self.r, 3)
---
+++ Алгоритмы Python/Part2/семинары/pract2/modules/task17/body_class.py
@@ -0,0 +1,16 @@
+from abc import ABCMeta, abstractmethod
+class BodyClass:
+    __metaclass__ = ABCMeta
+    @abstractmethod
+    def surface_area_calculation(self):
+        self.surface_area = 0
+    @abstractmethod
+    def volume_calculation(self):
+        self.volume = 0
+    def info(self, lclass=None):
+        a = self.surface_area
+        v = self.volume
+        if type(self.surface_area) == float or type(self.volume) == float:
+            a = str(round(self.surface_area, 2))
+            v = str(round(self.volume, 2))
+        print("\nВызов от {}\nПлощадь поверхности фигуры: {}\nОбъем фигуры: {}".format(lclass, a, v))
