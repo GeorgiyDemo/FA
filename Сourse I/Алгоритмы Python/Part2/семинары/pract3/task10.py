a85681908ecf0a862153dd009c5d2bd16b11816f
Georgiy Demo <demenchuk.george@protonmail.com>
2020-03-25 15:40:11 +0300

    Начало #30

---
+++ Сourse I/Алгоритмы Python/Part2/семинары/pract3/task10.py
@@ -0,0 +1,22 @@
+"""
+10. Даны числа a и b. Определите, сколько существует последовательностей из a нулей и b
+единиц, в которых никакие два нуля не стоят рядом
+"""
+
+public class Solution {
+    public static int recursion(int a, int b) {
+        // Базовый случай 
+        if (a > b + 1) {
+            return 0;
+        }
+        // Базовый случай 
+        if (a == 0 || b == 0) {
+            return 1;
+        }
+        // Шаг рекурсии / рекурсивное условие
+        return recursion(a, b - 1) + recursion(a - 1, b - 1);
+    }
+    public static void main(String[] args) {
+        System.out.println(recursion(5, 8)); // вызов рекурсивной функции
+    }
+}
