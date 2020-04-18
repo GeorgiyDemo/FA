88c6a21fc11fab29b1c9a79d243b7be36b24268a
Georgiy Demo <demenchuk.george@protonmail.com>
2020-04-18 19:29:08 +0300

    #33 класс работы есть практически

--- Сourse I/Алгоритмы Python/Part2/семинары/pract4/main.py
+++ Сourse I/Алгоритмы Python/Part2/семинары/pract4/main.py
@@ -11,13 +11,139 @@ import datetime
 class Work:
     """
     Класс работы
-
+    
+    При создании работы уже подразумевается то, что она выполнена
     !! К работе не относится экзамен
     """
-    def __init__(self, name, type, date):
-        allowed_list = ["практика", "контрольная", "тестирование"]
-        if type not in allowed_list:
+
+    WORK_COUNT = 6 #кол-во работ в полусеместре + 1 контрольная + 1 тестирование
+
+    def __init__(self, name, work_type, date_deadline, work_completed=False, date_completed=None, work_protected=False, date_protected=None):
+        """
+        Поля конструктора:
+        - name - название работы
+        - work_type - тип работы
+        - date_deadline - дата сдачи работы
+        
+        Необязательные:
+        - work_completed - сдача работы (True/False)
+        - date_completed - дата завершения сдачи работы
+        - work_protected - защита работы (True/False)
+        - date_protected - дата завершения защиты работы
+
+        """
+        
+        #Фильтрация входных данных
+        if work_type not in ["практика", "контрольная", "тестирование"]:
             raise ValueError("Некорректнй тип работы!")
+        
+        if not all(map(lambda x: type(x) == bool, [work_completed, work_protected])):
+            raise ValueError("Некорректные значения work_completed/work_protected!")
+        
+        if work_completed:
+            try:
+                self.date_completed = datetime.datetime.strptime(date_completed, "%d.%m.%Y")
+            except ValueError:
+                raise ValueError("Некорректный ввод даты сдачи работы!")
+        else:
+            self.date_completed = None
+        if work_protected:
+            try:
+                self.date_protected = datetime.datetime.strptime(date_protected, "%d.%m.%Y")
+            except ValueError:
+                raise ValueError("Некорректный ввод даты защиты работы!")
+        else:
+            self.date_protected = None
+        try:
+            self.date_deadline = datetime.datetime.strptime(date_deadline, "%d.%m.%Y")
+        except ValueError:
+            raise ValueError("Некорректный срок сдачи работы!")
+
+        #Выставление полей
+        self.points = 0
+        self.name = name
+        self.work_type = work_type
+        self.work_completed = work_completed
+        self.work_protected = work_protected
+    
+    def info(self):
+        """Информация о работе"""
+
+        sub_d = {True : "✅",False : "❌",}
+        d = {}
+        d["Название:"] = self.name
+        d["Тип работы:"] = self.work_type
+        d["Дедлайн:"] = self.date_deadline.strftime("%d.%m.%Y")
+        d["Завершение работы:"] = sub_d[self.work_completed]
+        d["Защита работы:"] = sub_d[self.work_protected]
+        d["Дата завершения работы:"] = self.date_completed.strftime("%d.%m.%Y") if self.date_completed != None else "-"
+        d["Дата защиты работы:"] = self.date_protected.strftime("%d.%m.%Y") if self.date_protected != None else "-"
+        d["Количество баллов:"] = self.points
+        return "*Класс работа*\n"+"\n".join([key+" "+str(value) for key, value in d.items()])
+
+    def set_complete(self, date):
+        """Осуществление сдачи работы"""
+
+        try:
+            date = datetime.datetime.strptime(date, "%d.%m.%Y")
+        except ValueError:
+            print("Некорректная дата сдачи работы!")
+            return
+
+        #Проверка на валидные данные
+        self.work_completed = True
+        self.date_completed = date
+
+    def set_protection(self, date):
+        """"Осуществление защиты работы и выставление баллов за работу"""
+        
+        try:
+            date = datetime.datetime.strptime(date, "%d.%m.%Y")
+        except ValueError:
+            print("Некорректная дата защиты работы!")
+            return
+
+        self.work_protected = True
+        self.date_protected = date
+
+        #Штрафные баллы
+        penalty_points = 0
+        #Балл за работу
+        points = 20/Work.WORK_COUNT
+
+        #Вычисление коэффа понижения кол-ва баллов. Каждая неделя - 1 балл
+        if date > self.date_deadline:
+            diff = self.date_deadline-date
+            penalty_points = int(diff.days/7)
+            
+            #Если принесли работу так поздно, что потеряли все, то 0 баллов
+            if penalty_points > points:
+                penalty_points = points
+
+        self.points = points-penalty_points
+
+class Certification:
+    """
+    Класс промежуточной аттестации (по логике 2 промежуточных = полная аттестация за семестр)
+    
+    - Добавление баллов по практической        <- это все один метод, который в зависимости от типа, добавляет объект работы в list
+    - Добавление баллов по контрольной
+    - Добавление баллов по тестированию
+    """
+    points_counter = 0
+
+    def __init__(self, name):
+        self.work_obj_list = []
+
+    
+    def work_add(self, work_obj):
+        """Метод для добавления работы к промежуточной аттестации"""
+        if not isinstance(work_obj, Work):
+            raise ValueError("Объект не класса Work!")
+        #Добавление работы к промежуточной аттестации
+        self.work_obj_list.append(work_obj)
+
+
 
 class Student:
     """
@@ -28,9 +154,9 @@ class Student:
     - тестирование (тесты)
     
     Методы:
-    - Добавление баллов по практической        <- это все один метод, который в зависимости от типа, добавляет объект работы в list
-    - Добавление баллов по контрольной
-    - Добавление баллов по тестированию
+    - Добавление полусеместра
+    - Добавление второго полусеместра
+    - Добавление экзамена
 
     - Добавление баллов по экзамену
     - Получение общего кол-ва баллов
@@ -49,27 +175,6 @@ class Student:
         """Метод добавления аттестации"""
         pass
 
-
-
-
-#########################################
-class Certification:
-    """
-    Класс аттестации
-
-    - Добавление баллов по практической        <- это все один метод, который в зависимости от типа, добавляет объект работы в list
-    - Добавление баллов по контрольной
-    - Добавление баллов по тестированию
-
-    """
-    def __init__(self, student_name)
-
-class IntermediateCertification(Certification):
-    """
-    Класс промежуточной аттестации 
-    """
-class 
-
 def main():
     """
     Общий алгоритм
@@ -85,3 +190,13 @@ def main():
 
     В конце, в методе получения итоговой оценки, надо понять, что 
     """
+    obj = Work("№1 Рекурсия", "практика", "20.03.2020")
+    print(obj.info())
+    obj.set_complete("19.03.2020")
+    print(obj.info())
+    obj.set_protection("21.03.2020")
+    print(obj.info())
+
+
+if __name__ == "__main__":
+    main()
