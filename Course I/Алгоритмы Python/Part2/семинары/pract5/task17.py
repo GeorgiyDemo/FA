e34f66605b94f607a046de26e7361609862410b2
Georgiy Demo <demenchuk.george@protonmail.com>
2020-04-28 22:59:58 +0300

    18 есть #35
    Оно и правда простое

--- Course I/Алгоритмы Python/Part2/семинары/pract5/task17.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract5/task17.py
@@ -3,3 +3,25 @@
 Сдвинуть элементы массива в указанном направлении (влево или вправо) и на указанное число
 шагов. Освободившиеся ячейки заполнить нулями. Выводить массив после каждого шага.
 """
+import numpy as np
+from random import randint
+def main():
+    try:
+        n = int(input("Введите кол-во элементов в массиве -> "))
+    except ValueError:
+        print("Некорректный ввод данных!")
+        return
+    np_arr = np.array([randint(-100,100) for _ in range(n)])
+    print("Исходный массив:", np_arr)
+
+    #Индекс
+    itemindex = np.where(np_arr < 0)[0][0]
+    print("Индекс первого отрицательного элемента: {}".format(itemindex))
+    #Новый массив
+    new_arr = np_arr[itemindex+1:]
+    #Сумма по молулю
+    s = np.sum((np.absolute(new_arr)))
+    print("Сумма модулей после отрицательного элемента: {}".format(s))
+
+if __name__ == "__main__":
+    main()
