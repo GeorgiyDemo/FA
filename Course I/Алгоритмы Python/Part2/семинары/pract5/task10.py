ae95538b2c68eeaa434bbcd494138115caa68b5e
Georgiy Demo <demenchuk.george@protonmail.com>
2020-04-28 22:23:01 +0300

    10 есть #35

--- Course I/Алгоритмы Python/Part2/семинары/pract5/task10.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract5/task10.py
@@ -8,28 +8,37 @@ class MainClass:
     def __init__(self):
         self.processing()
     
+    def digital_checker(self, n):
+        try:
+            return int(n)
+        except ValueError:
+            return n
 
     def processing(self):
 
         base_list1 = []
         base_list2 = [] 
         try:
-            n1 = int(input("Введите размернсоть массива №1 -> "))
-            n2 = int(input("Введите размернсоть массива №2 -> "))
+            n1 = int(input("Введите размерноcть массива №1 -> "))
+            n2 = int(input("Введите размерность массива №2 -> "))
         except ValueError:
             print("Некорректный ввод данных!")
             return
         
-        print("*Заполнение массива №1*")
+        print("\n*Заполнение массива №1*")
         for i in range(n1):
-            input("Введите элемент №{} -> ".format(i+1))
-        arr1 = np.array([1,2,3,4,5])
-        arr2 = np.array([1,2,3,4,3,5,3])
-        r = np.array_equal(np.unique(arr1), np.unique(arr2))
-        print()
+            base_list1.append(self.digital_checker(input("Введите элемент №{} -> ".format(i+1))))
+        
+        print("\n*Заполнение массива №2*")
+        for i in range(n2):
+            base_list2.append(self.digital_checker(input("Введите элемент №{} -> ".format(i+1))))
+        
+        print("\nВведенный массив №1: {}\nВведенный массив №2: {}".format(base_list1, base_list2))
+        print("\nРезультат: {}".format(self.comparer(base_list1, base_list2)))
     
     def comparer(self, arr1, arr2):
-        return np.array_equal(np.unique(arr1), np.unique(arr2))
+        d = { True : "массивы совпадают", False : "массивы НЕ совпадают"}
+        return d[np.array_equal(np.unique(arr1), np.unique(arr2))]
 
 if __name__ == "__main__":
-    pass
+    MainClass()
