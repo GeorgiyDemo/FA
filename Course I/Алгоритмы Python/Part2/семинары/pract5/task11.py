ad66ad0ffca3ac05da372f93922f964ce39235e2
Georgiy Demo <demenchuk.george@protonmail.com>
2020-04-28 22:54:07 +0300

    11 есть адекватный #36

--- Course I/Алгоритмы Python/Part2/семинары/pract5/task11.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract5/task11.py
@@ -2,3 +2,13 @@
 Задача 11:
 Дан список/массив. После каждого элемента добавьте предшествующую ему часть списка.
 """
+import numpy as np
+
+#a = [int(i) for i in input("Список: ").split()]
+arr_a = np.array([1, 2, 3]) #[1, 1, 2, 1, 2, 3]
+arr_b = np.array([])
+print("Исходный массив:", arr_a)
+for i in range(len(arr_a)):
+    for j in range(i+1):
+        arr_b = np.append(arr_b, arr_a[j])
+print("Результат:", arr_b)
--- Course I/Алгоритмы Python/Part2/семинары/pract5/task12.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract5/task12.py
@@ -3,3 +3,8 @@
 Пусть элементы списка/массива хранят символы предложения. Замените каждое вхождение слова
 "itmathrepetitor" на "silence".
 """
+import numpy as np
+np_arr = np.array([*list("привет itmathrepetitor мяв")])
+for i 
+arr = np.where(np_arr =="itmathrepetitor", "silence", np_arr)
+print(arr)
