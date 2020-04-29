f856985d99d3941b7367571fce5db44a0d37d1b2
Georgiy Demo <demenchuk.george@protonmail.com>
2020-04-29 16:46:16 +0300

    необходимость переписать 12 #35

--- Course I/Алгоритмы Python/Part2/семинары/pract5/task12.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract5/task12.py
@@ -4,7 +4,49 @@
 "itmathrepetitor" на "silence".
 """
 import numpy as np
-np_arr = np.array([*list("привет itmathrepetitor мяв")])
-for i 
-arr = np.where(np_arr =="itmathrepetitor", "silence", np_arr)
-print(arr)
+
+class MainClass(object):
+
+    def __init__(self):
+        input_str = input("Введите строку для замены 'itmathrepetitor' на 'silence' -> ")
+        self.arr = np.array(list(input_str))
+        self.sub_arr = np.array(list("itmathrepetitor"))
+        self.replace_list = np.array(list("silence"))
+        self.processing()
+
+    def get_sublist_index(self):
+
+        sub = self.sub_arr
+        arr = self.arr
+        sublen = len(sub)
+        print(sub[0])
+        first = sub[0] if sub else []
+        indx = -1
+
+        while True:
+            try:
+                indx = arr.index(first, indx + 1)
+            except ValueError:
+                break
+            if sub == arr[indx: indx + sublen]:
+                return True, indx, indx + len(sub)
+        return False, 0, 0
+
+    def processing(self):
+
+        print("Список до замены:\n{}".format(self.arr))
+        processing_flag = True
+
+        while processing_flag:
+            index_tuple = self.get_sublist_index()
+            if index_tuple[0]:
+                print("Замена подсписка по индексам", index_tuple[1], index_tuple[2])
+                del self.arr[index_tuple[1]:index_tuple[2]]
+                self.arr[index_tuple[1]:index_tuple[1]] = self.replace_list
+            else:
+                processing_flag = False
+
+        print("Список после замены:\n" + str(self.arr))
+
+if __name__ == "__main__":
+    MainClass()
