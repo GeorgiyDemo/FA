d601fe83890df0307ded1e4eecdc2a78f70ddb41
Georgiy Demo <demenchuk.george@protonmail.com>
2020-04-30 00:08:40 +0300

    Каркас 16 #35

--- Course I/Алгоритмы Python/Part2/семинары/pract5/task16.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract5/task16.py
@@ -3,3 +3,29 @@
 Выполнить программную реализацию калькулятора матриц.
 """
 import numpy as np
+
+class MatrixClass:
+    def __init__(self, value):
+        self.value = np.matrix(value)
+
+    def __str__(self):
+        """Вывод на экран"""
+        pass
+    
+    def __mul__(self):
+        """Умножение матриц"""
+    
+    
+
+class ClacClass():
+    def __init__(self, matrix1=None, matrix2=None):
+        if matrix1 == None and matrix2 == None:
+            self.generator()
+        else:
+            self.matrix1 = matrix1
+            self.matrix2 = matrix2
+
+
+if __name__ == "__main__":
+    #ClacClass
+    
