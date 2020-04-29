2a01ba6343b62d9d0aeb3883a68438e7ccd48a5a
Georgiy Demo <demenchuk.george@protonmail.com>
2020-04-29 11:03:27 +0300

    Скелет 20 и лютые методы numpy в 9 #35

--- Course I/Алгоритмы Python/Part2/семинары/pract5/task20.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract5/task20.py
@@ -4,3 +4,35 @@
 ячейки третьего записать суммы соответствующих ячеек первых двух. Вывести содержимое
 массивов на экран.
 """
+
+import numpy as np
+from random import randint
+def input_N():
+    """Ввод числа N"""
+    try:
+        N = int(input("Длина массивов -> "))
+        return N
+    except ValueError:
+        print("Вводить число!")
+        return input_N()
+
+N = input_N()
+rand_arr = np.array([randint(-100, 100) for _ in range(N)])
+print("Рандомный массив:", rand_arr)
+# заменить на ручной ввод
+my_arr = np.array([])
+for i in range(N):
+    my_arr = np.append(my_arr, 0)
+print("Заполните массив:")
+for i in range(N):
+    my_arr[i] = int(input())
+print("Вводный массив:", my_arr)
+
+arr_sum = np.array([])
+for i in range(N):
+    arr_sum = np.append(arr_sum, 0)
+
+for i in range(N):
+    arr_sum[i] = rand_arr[i] + my_arr[i]
+
+print(arr_sum)
--- Course I/Алгоритмы Python/Part2/семинары/pract5/task9.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract5/task9.py
@@ -4,3 +4,27 @@
 а) положительные числа;
 б) элементы с четными порядковыми номерами в списке.
 """
+import numpy as np
+from random import randint
+def main():
+    try:
+        n = int(input("Введите кол-во элементов в массиве -> "))
+    except ValueError:
+        print("Некорректный ввод данных!")
+        return
+    np_arr = np.array([randint(-100,100) for _ in range(n)])
+    print("Исходный массив:", np_arr)
+
+    #Упорядочить по возрастанию только положительные числа
+    
+
+
+    np_arr = np.vectorize(lambda x: x > 0, np_arr)
+    print(np.array([np_arr]))
+    arr3 = np.argsort(np_arr)
+    print(arr3)
+ 
+    #print(np.sort(np_arr, 0, np.where(np_arr < 0)))
+
+if __name__ == "__main__":
+    main()
