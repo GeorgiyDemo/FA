c69bf08bc2b6f798c4679a67c9c4913ba468de49
Georgiy Demo <demenchuk.george@protonmail.com>
2020-04-30 18:48:35 +0300

    Смещение #35

--- Course I/Алгоритмы Python/Part2/семинары/pract5/task17.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract5/task17.py
@@ -5,23 +5,52 @@
 """
 import numpy as np
 from random import randint
-def main():
-    try:
-        n = int(input("Введите кол-во элементов в массиве -> "))
-    except ValueError:
-        print("Некорректный ввод данных!")
-        return
-    np_arr = np.array([randint(-100,100) for _ in range(n)])
-    print("Исходный массив:", np_arr)
 
-    #Индекс
-    itemindex = np.where(np_arr < 0)[0][0]
-    print("Индекс первого отрицательного элемента: {}".format(itemindex))
-    #Новый массив
-    new_arr = np_arr[itemindex+1:]
-    #Сумма по молулю
-    s = np.sum((np.absolute(new_arr)))
-    print("Сумма модулей после отрицательного элемента: {}".format(s))
+class ChangerClass:
+    def __init__(self):
+        try:
+            n = int(input("Введите кол-во элементов в массиве -> "))
+        except ValueError:
+            print("Некорректный ввод данных!")
+            return
+        np_arr = np.array([randint(-100,100) for _ in range(n)])
+
+        print("Исходный массив:", np_arr)
+        self.np_arr = np_arr
+        
+        while True:
+            try:
+
+                self.offset = int(input("Введите кол-во шагов для смещения -> "))
+                offset_input = input("В какую стороны вы хотите сдвинуть элементы массива?\n1. Влево\n2. Вправо\n0. Выход из программы\n-> ")
+                if offset_input == "1":
+                    self.left_offset()
+                elif offset_input == "2":
+                    self.right_offset()
+                if offset_input == "0":
+                    break
+                else:
+                    raise ValueError
+            
+            except ValueError:
+                print("Некорректный ввод данных")
+        
+    def right_offset(self):
+        """Смещение вправо"""
+        arr = self.np_arr
+        offset = self.offset
+    
+    def left_offset(self):
+        """Смещение влево"""
+
+        arr = self.np_arr
+        offset = self.offset
+        #Обрезаем c помощью slices
+        new_arr = arr[offset:]
+        #Добавляем нули
+        new_arr = np.pad(new_arr, (0, arr.shape[0] - new_arr.shape[0]))
+        print("Массив после смещения на {}:\n{}".format(offset, new_arr))
+        self.np_arr = new_arr
 
 if __name__ == "__main__":
-    main()
+    ChangerClass()
