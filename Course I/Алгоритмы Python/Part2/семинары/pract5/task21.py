f572f77e0669a5b07b6a6988fee2db8d1d41dbc7
Georgiy Demo <demenchuk.george@protonmail.com>
2020-05-01 12:24:59 +0300

    Сравнение list/numpy/array #35

--- Course I/Алгоритмы Python/Part2/семинары/pract5/task21.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract5/task21.py
@@ -4,21 +4,46 @@
 массива. Посчитать сколько среди них положительных, отрицательных и нулевых значений.
 Вывести на экран элементы массива и посчитанные количества. Рассмотрите возможность
 использования numpy.set_printoptions для полноценного вывода занчений массива на экран.
+
+List
+Положительных: 7998842
+Отрицательных: 10001230
+Равных нулю: 1999929
+Время: 2.9530217049999976
+
+Numpy
+Положительных: 7996617
+Отрицательных: 10002649
+Равных нулю: 2000735
+Время: 0.47020314399999563
 """
 
 import sys
 import numpy as np
 from random import randint
+import timeit
 
-def main():
+def default():
+    l = [randint(-5, 4) for _ in range(2001)]
+    #print("Элементы списка:\n{}".format(l))
+    print("List")
+    a = timeit.default_timer()
+    print("Положительных: {}".format(len([e for e in l if e > 0])))
+    print("Отрицательных: {}".format(len([e for e in l if e < 0])))
+    print("Равных нулю: {}".format(len([e for e in l if e == 0])))
+    print("Время: {}".format(timeit.default_timer()-a))
 
+def numpy():
     arr = np.array([randint(-5, 4) for _ in range(2001)])
     np.set_printoptions(threshold=sys.maxsize)
-
-    print("Элементы массива:\n{}".format(arr))
+    #print("Элементы массива:\n{}".format(arr))
+    print("\nNumpy:")
+    a = timeit.default_timer()
     print("Положительных: {}".format(arr[arr > 0].shape[0]))
     print("Отрицательных: {}".format(arr[arr < 0].shape[0]))
     print("Равных нулю: {}".format(arr[arr == 0].shape[0]))
+    print("Время: {}".format(timeit.default_timer()-a))
 
 if __name__ == "__main__":
-    main()
+    default()
+    numpy()
