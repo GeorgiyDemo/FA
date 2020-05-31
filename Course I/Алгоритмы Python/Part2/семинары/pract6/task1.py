6b3b628aca66c671a2e7a4e5c9a57461e637fafd
Georgiy Demo <demenchuk.george@protonmail.com>
2020-05-31 18:31:55 +0300

    Cisco pkt - изменения

--- Course I/Алгоритмы Python/Part2/семинары/pract6/task1.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract6/task1.py
@@ -1,3 +1,47 @@
 """
 Выполнить представление через множества и ленточное представления бинарного дерева, представленного на рис. 1
 """
+
+# Фрагмент реализации бинарного дерева с помощью представления в виде узлов и ссылок:
+class BinaryTree:
+    def __init__(self, root):
+        self.key = root
+        self.left_child = None
+        self.right_child = None
+        
+    def insert_left(self,new_node):
+        if self.left_child == None:
+            self.left_child = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.left_child = self.left_child
+            self.left_child = t
+            
+    def insert_right(self,new_node):
+        if self.right_child == None:
+            self.right_child = BinaryTree(new_node)
+        else:
+            t = BinaryTree(new_node)
+            t.right_child = self.right_child
+            self.right_child = t
+    
+    def get_right_child(self):
+        return self.right_child
+
+    def get_left_child(self):
+        return self.left_child
+
+    def set_root_val(self,obj):
+        self.key = obj
+
+    def get_root_val(self):
+        return self.key
+    
+    def __str__(self):
+        return '{} ({}, {})'.format(self.get_root_val(), str(self.get_left_child()), str(self.get_right_child()))
+
+if __name__ == "__main__":
+    r = BinaryTree('8')
+    r.insert_left('4')
+    r.insert_right('12')
+    print(r)
