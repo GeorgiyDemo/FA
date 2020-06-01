cf8174f9c6395ca49d4ac814c9e6ec1ea66b4395
Georgiy Demo <demenchuk.george@protonmail.com>
2020-06-01 15:22:38 +0300

    Бейкапчик

--- Course I/Алгоритмы Python/Part2/семинары/pract6/task4/task.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract6/task4/task.py
@@ -4,9 +4,12 @@
 
 1. Реализовать представление данных с помощью бинарного дерева;
 
+
 2. Реализовать поиск элементов в массиве с учетом времени на поиск
+
 3. Реализовать поиск элемента в бинарном дереве с учетом времени на
 поиск
+
 4. Реализовать поиск элементов в отсортированном массиве с учетом
 времени на поиск
 
@@ -14,41 +17,25 @@
 программных реализациях.
 
 """
+# A merge sort algorithm that is probably bigger than it needs to be.
+# Uses binary trees to sort integers.
+# Runtime is probably O(NlogN)
 
-class BinaryTree:
-    """Класс бинарного дерева"""
+from binarytree
+class Node:
 
-    def __init__(self, root=None, parent=None):
-        self.key = root
-        self.parent = parent
+    def __init__(self, value=None):
+        self.value = value
         self.left_child = None
         self.right_child = None
 
-    def insert_left(self, new_node=None):
-        """Вставка элемента слева"""
-        if self.left_child == None:
-            self.left_child = BinaryTree(new_node, self)
-        else:
-            t = BinaryTree(new_node, self)
-            t.left_child = self.left_child
-            self.left_child = t
-        
-        return self.left_child
+    def set_root_val(self, obj):
+        """Выставление значения"""
+        self.value = obj
 
-    def insert_right(self, new_node=None):
-        """Вставка элемента справа"""
-        if self.right_child == None:
-            self.right_child = BinaryTree(new_node, self)
-        else:
-            t = BinaryTree(new_node, self)
-            t.right_child = self.right_child
-            self.right_child = t
-
-        return self.right_child
-
-    def get_parent(self):
-        """Получение узла-родителя"""
-        return self.parent
+    def get_root_val(self):
+        """Получение значения"""
+        return self.value
 
     def get_right_child(self):
         """Получение подэлемента справа"""
@@ -58,38 +45,86 @@ class BinaryTree:
         """Получение подэлемента слева"""
         return self.left_child
 
-    def set_root_val(self, obj):
-        """Выставление значения"""
-        self.key = obj
+    def compare(self, new_value):
+        """Сравнение значений"""
+        # If the value is greater or equal
+        # Try stick it in the right
+        if new_value >= self.value:
+            if self.right_child:
+                self.right_child.compare(new_value)
+            else:
+                # Create a node if there is no right yet.
+                self.right_child = Node(new_value)
 
-    def get_root_val(self):
-        """Получение значения"""
-        return self.key
+        if new_value < self.value:
+            # If the value is less
+            # Try stick it in the left
+            if self.left_child:
+                self.left_child.compare(new_value)
+            else:
+                # If there is nothing there yet create it
+                self.left_child = Node(new_value)
+
+    def sort(self):
+        """Сортировка данных"""
+        if self.left_child:
+            self.left_child = self.left_child.sort()
+        else:
+            # Otherwise I'm the farthest left
+            self.left_child = []
+
+        # If there is something on the right: tell it to sort
+        if self.right_child:
+            self.right_child = self.right_child.sort()
+        else:
+            # Otherwise I'm the farthest right
+            self.right_child = []
+
+        # Return the sorted left children, me, and the sorted right children.
+        return self.left_child + [self.value] + self.right_child
 
     def __str__(self):
         """Вывод структуры на экран"""
 
-        left_val = "-" if self.get_left_child() is None else self.get_left_child()
-        right_val = "-" if self.get_right_child() is None else self.get_right_child()
+        left_val = "-" if self.left_child is None else self.left_child
+        right_val = "-" if self.right_child is None else self.right_child
 
-        return '{} ({}, {})'.format(self.get_root_val(), left_val, right_val)
-
-
-def main():
-    data = [3,5,2,1,4,6,7,8,9,10,11,12,13,14]
-    tree = BinaryTree(data[0])
-    main_tree = tree
-    for e in data[1:]:
-        if tree.get_root_val() > e:
-            tree = tree.insert_left(e)
-        elif tree.get_root_val() < e:
-            tree = tree.insert_right(e)
-
-    print(main_tree)
+        return '{} ({}, {})'.format(self.value, left_val, right_val)
 
 
+class SortedTree:
 
+    def __init__(self):
+        self.root = None
 
+    def push(self, value):
+        if self.root:
+            # If we have a root then throw the value in to compare it.
+            self.root.compare(value)
+        else:
+            # Otherwise start with a new root.
+            # This could all be done in the __init__ tbf
+            self.root = Node(value)
 
-if __name__ == "__main__":
-    main()
+    def merge(self):
+        return self.root.sort()
+
+    def __str__(self):
+        return self.root.__str__()
+
+    
+# Let's do this!
+
+# Create a semi-random but actually human-biased array of integers.
+items = [81, 77, 79, 68, 10, 12, 13, 20, 15, 24, 27, 42, 33, 51, 57]
+
+# Instantiate the top of the tree
+tree = SortedTree()
+# Push each item into the tree (this is the first N in O(NlogN))
+for value in items:
+    tree.push(value)
+
+print(tree)
+# Merge the results (this is the logN bit)
+# Funny thing: this will break if called immediately after instantiating.
+print(tree.merge())
