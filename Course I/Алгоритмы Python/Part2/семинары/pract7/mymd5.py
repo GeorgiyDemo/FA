ffdc6c0b5daa49c45a5470e2bfd5e87cf3c074a2
Georgiy Demo <demenchuk.george@protonmail.com>
2020-06-06 17:32:27 +0300

    Добавление комментов #41

--- Course I/Алгоритмы Python/Part2/семинары/pract7/mymd5.py
+++ Course I/Алгоритмы Python/Part2/семинары/pract7/mymd5.py
@@ -1,15 +1,22 @@
 """
 Выполнить собственную программную реализацию MD5.
+Референс: https://ru.wikipedia.org/wiki/MD5
 """
 
 import struct
 from enum import Enum
 from math import floor, sin
-
 from bitarray import bitarray
 
 
 class MD5Buffer(Enum):
+    """
+    Буфер начальных значений MD5, используется на 3 шаге
+
+    Для вычислений инициализируются 4 переменных размером по 32 бита
+    задаются начальные значения шестнадцатеричными числами
+    (порядок байтов little-endian, сначала младший байт):
+    """
     A = 0x67452301
     B = 0xEFCDAB89
     C = 0x98BADCFE
@@ -17,6 +24,8 @@ class MD5Buffer(Enum):
 
 
 class MD5:
+    """Класс MD5"""
+
     string = None
     buffers = {
         MD5Buffer.A: None,
@@ -27,6 +36,9 @@ class MD5:
 
     @classmethod
     def hash(cls, string):
+        """
+        Основной метод для вызова и получения данных
+        """
         cls.string = string
 
         preprocessed_bit_array = cls.step_2(cls.step_1())
@@ -36,21 +48,29 @@ class MD5:
 
     @classmethod
     def step_1(cls):
-        """Конвертация строки в bit array"""
+        """
+        Шаг 1. Выравнивание потока
+        (конвертация строки в bit array)
+        """
         bit_array = bitarray(endian="big")
         bit_array.frombytes(cls.string.encode("utf-8"))
 
+        # Сначала к концу потока дописывают единичный бит.
         bit_array.append(1)
+
+        # Затем добавляют некоторое число нулевых бит такое, чтобы новая длина потока стала сравнима с 448 по модулю 512
         while bit_array.length() % 512 != 448:
             bit_array.append(0)
 
-        """Перевод в нижний регистр"""
         return bitarray(bit_array, endian="little")
 
     @classmethod
     def step_2(cls, step_1_result):
-        # Extend the result from step 1 with a 64-bit little endian
-        # representation of the original message length (modulo 2^64).
+        """
+        Шаг 2. Добавление длины сообщения
+        """
+
+        # В конец сообщения дописывают 64-битное представление длины данных (количество бит в сообщении) до выравнивания
         length = (len(cls.string) * 8) % pow(2, 64)
         length_bit_array = bitarray(endian="little")
         length_bit_array.frombytes(struct.pack("<Q", length))
@@ -61,55 +81,79 @@ class MD5:
 
     @classmethod
     def step_3(cls):
-        """Буферы со значениями по-умолчанию"""
+        """
+        Шаг 3. Инициализация буфера
+        (буферы со значениями по-умолчанию)
+        """
         for buffer_type in cls.buffers.keys():
             cls.buffers[buffer_type] = buffer_type.value
 
     @classmethod
     def step_4(cls, step_2_result):
+        """
+        Шаг 4. Вычисление в цикле
+        """
 
-        F = lambda x, y, z: (x & y) | (~x & z)
-        G = lambda x, y, z: (x & z) | (y & ~z)
-        H = lambda x, y, z: x ^ y ^ z
-        I = lambda x, y, z: y ^ (x | ~z)
+        # 1-й раунд
+        def F(x, y, z): return (x & y) | (~x & z)
+        # 2-й раунд
+        def G(x, y, z): return (x & z) | (y & ~z)
+        # 3-й раунд
+        def H(x, y, z): return x ^ y ^ z
+        # 4-й раунд
+        def I(x, y, z): return y ^ (x | ~z)
 
-        rotate_left = lambda x, n: (x << n) | (x >> (32 - n))
-        modular_add = lambda a, b: (a + b) % pow(2, 32)
+        def rotate_left(x, n): return (x << n) | (x >> (32 - n))
+        def modular_add(a, b): return (a + b) % pow(2, 32)
 
+        # Определение таблицы констант T
         T = [floor(pow(2, 32) * abs(sin(i + 1))) for i in range(64)]
 
         N = len(step_2_result) // 32
 
         for chunk_index in range(N // 16):
+
             start = chunk_index * 512
-            X = [step_2_result[start + (x * 32): start + (x * 32) + 32] for x in range(16)]
+            # Каждый 512-битный блок проходит 4 этапа вычислений по 16 раундов. Для этого блок представляется в виде массива X из 16 слов по 32 бита.
+            X = [step_2_result[start + (x * 32): start + (x * 32) + 32]
+                 for x in range(16)]
+            # Переводим все в int из байтов
+            X = [int.from_bytes(word.tobytes(), byteorder="little")
+                 for word in X]
 
-            X = [int.from_bytes(word.tobytes(), byteorder="little") for word in X]
-
+            # Получаем значение из буферов
             A = cls.buffers[MD5Buffer.A]
             B = cls.buffers[MD5Buffer.B]
             C = cls.buffers[MD5Buffer.C]
             D = cls.buffers[MD5Buffer.D]
 
             for i in range(4 * 16):
+
+                # 1-й раунд
                 if 0 <= i <= 15:
                     k = i
                     s = [7, 12, 17, 22]
                     buff = F(B, C, D)
+
+                 # 2-й раунд
                 elif 16 <= i <= 31:
                     k = ((5 * i) + 1) % 16
                     s = [5, 9, 14, 20]
                     buff = G(B, C, D)
+
+                # 3-й раунд
                 elif 32 <= i <= 47:
                     k = ((3 * i) + 5) % 16
                     s = [4, 11, 16, 23]
                     buff = H(B, C, D)
+
+                # 4-й раунд
                 elif 48 <= i <= 63:
                     k = (7 * i) % 16
                     s = [6, 10, 15, 21]
                     buff = I(B, C, D)
 
-                # Тут происходит модульное значение
+                # Тут происходит добавление модульного значения
                 buff = modular_add(buff, X[k])
                 buff = modular_add(buff, T[i])
                 buff = modular_add(buff, A)
@@ -130,6 +174,11 @@ class MD5:
 
     @classmethod
     def step_5(cls):
+        """
+        Шаг 5. Результат вычислений
+        Результат вычислений находится в буфере ABCD, это и есть хеш.
+        Если выводить побайтово, начиная с младшего байта A и закончив старшим байтом D, то мы получим MD5-хеш.
+        """
         # Конвертация в прямой порядок байтов
         A = struct.unpack("<I", struct.pack(">I", cls.buffers[MD5Buffer.A]))[0]
         B = struct.unpack("<I", struct.pack(">I", cls.buffers[MD5Buffer.B]))[0]
