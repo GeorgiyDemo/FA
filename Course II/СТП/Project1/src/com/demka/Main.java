0cebd55695b8d2f2a9c0800054982df7a7c839b8
Georgiy Demo <demenchuk.george@protonmail.com>
2020-09-02 14:33:09 +0300

    Начало по java #44

--- Course II/СТП/Project1/src/com/demka/Main.java
+++ Course II/СТП/Project1/src/com/demka/Main.java
@@ -1,37 +1,59 @@
 package com.demka;
 
+import java.util.*;
 public class Main {
 
-    public static int returner(int i)
-    {
-        return i*2;
+    /*
+    Метод для рассчета гипотенузы по заданию №5
+    На вход принимает 2 катета в double
+     */
+    public static double HypotenuseCalculator(double cathetus1, double cathetus2){
+        return Math.sqrt( Math.pow(cathetus1, 2.0) + Math.pow(cathetus2, 2.0));
     }
 
-    public static void main(String[] args)
-    {
-        //Объявление переменной
-        int a = 1;
-        System.out.println(a);
-        a++;
-        System.out.println(a);
-        a = a + 1;
-        System.out.println(a);
+    public static void main(String[] args) {
+        //1. Вывести текст
+        System.out.println("Hello world");
 
+        //2. Создайте переменную, присвойте ей целочисленное значение. Выведите на экран
+        int testVar = 42;
+        System.out.println(testVar);
 
+        //3. Создайте переменную, увеличьте ее несколькими способами и выведите её на экран
+        int testVar1 = 1;
+        System.out.println("Начальное значение: "+testVar1);
+        testVar1++;
+        System.out.println("Начальное значение +1: "+testVar1);
+        testVar1 = testVar1 + 1;
+        System.out.println("Начальное значение +1: "+testVar1);
+        ++testVar1;
+        System.out.println("Начальное значение +1: "+testVar1);
 
-        //Вывод
-        System.out.println("Привет");
+        //4. Даны две переменные. Поменяйте значения переменных друг с другом двумя способами
+        int a = 5;
+        int b = 2;
+        System.out.println("\nA = "+a);
+        System.out.println("B = "+b);
 
-        //Цикл
-        /*
-        for (int i = 0; i < 100; i++)
-        {
-            System.out.println(returner(i));
-        }
+        int buffer = a;
+        a = b;
+        b = buffer;
+        System.out.println("A = "+a);
+        System.out.println("B = "+b);
 
-         */
+        a=a+b-(b=a);
+        System.out.println("A = "+a);
+        System.out.println("B = "+b);
 
-        //Арифметические операции
+        a += b;
+        b = a - b;
+        a -= b;
+        System.out.println("A = "+a);
+        System.out.println("B = "+b);
 
+        //5. Дано два числа a и b. Найдите гипотенузу треугольника с заданными катетами
+        double side1 = 3.4;
+        double side2 = 4.6;
+        System.out.println(HypotenuseCalculator(side1, side2));
     }
 }
