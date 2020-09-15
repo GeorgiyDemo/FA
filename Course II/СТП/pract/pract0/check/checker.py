324c315089506b91baceae0c72b5354ef06a2f77
Georgiy Demo <demenchuk.george@protonmail.com>
2020-09-15 13:36:04 +0300

    Задание по видосу
    https://www.youtube.com/watch?v=ovJcsL7vyrk

---
+++ Course II/СТП/pract/pract0/check/checker.py
@@ -0,0 +1,13 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open ("./out.txt", "r") as file:
+    data = file.read()
+
+data = data.split("\n")
+data = list(map(float, data))
+
+plt.clf()
+plt.plot(data)
+plt.show()
