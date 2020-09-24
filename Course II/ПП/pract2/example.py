6c416cd9abc6bacc1ac46b59852820e0cd707971
Georgiy Demo <demenchuk.george@protonmail.com>
2020-09-24 11:58:10 +0300

    Лаба 2 tkinter #55

--- Course II/ПП/pract2/example.py
+++ Course II/ПП/pract2/example.py
@@ -1,45 +1,48 @@
-# Python program to implement 
-# Superellipse 
+# Python program to implement
+# Superellipse
 
-# importing the required libraries 
-import matplotlib.pyplot as plt 
-from math import sin, cos, pi 
+# importing the required libraries
+import matplotlib.pyplot as plt
+from math import sin, cos, pi
 
-def sgn(x): 
-	return ((x>0)-(x<0))*1
+
+def sgn(x):
+    return ((x > 0) - (x < 0)) * 1
+
 
 def processing(a, b, n):
-    na = 2 / n 
-    # defining the accuracy 
+    na = 2 / n
+    # defining the accuracy
     step = 100
-    piece =(pi * 2)/step 
-    xp =[]
-    yp =[] 
+    piece = (pi * 2) / step
+    xp = []
+    yp = []
     t = 0
-    for t1 in range(step + 1): 
-        # because sin ^ n(x) is mathematically the same as (sin(x))^n... 
-        x =(abs((cos(t)))**na)*a * sgn(cos(t)) 
-        y =(abs((sin(t)))**na)*b * sgn(sin(t)) 
+    for t1 in range(step + 1):
+        # because sin ^ n(x) is mathematically the same as (sin(x))^n...
+        x = (abs((cos(t))) ** na) * a * sgn(cos(t))
+        y = (abs((sin(t))) ** na) * b * sgn(sin(t))
         xp.append(x)
-        yp.append(y) 
-        t+= piece 
+        yp.append(y)
+        t += piece
 
     print(xp)
-    plt.plot(xp, yp) # plotting all point from array xp, yp 
+    plt.plot(xp, yp)  # plotting all point from array xp, yp
 
 
 def main():
-    
+
     n = 0.4
     while n != 2.5:
-    
+
         a, b = 200, 200
-        processing(a,b,n)
-        n  = round(n + 0.1, 1)
+        processing(a, b, n)
+        n = round(n + 0.1, 1)
         break
 
-    plt.title("Superellipse with parameter "+str(n)) 
-    plt.show() 
+    plt.title("Superellipse with parameter " + str(n))
+    plt.show()
+
 
 if __name__ == "__main__":
     main()
--- Course II/ПП/pract2/task1.py
+++ Course II/ПП/pract2/task1.py
@@ -1,38 +1,48 @@
 import tkinter as tk
-from math import sin, cos, pi 
+from math import sin, cos, pi
 
-def drawer(canvas, x, y):
+
+def drawer(canvas, x, y, a, b):
     """Создание точки"""
-    #canvas.create_line(x, y, x+1, y, fill="black")
-    canvas.create_line(x, y, x+1, y, fill="black")
+    # Смещение
+    x = a + x
+    y = b + y
 
-def sgn(x): 
-	return ((x>0)-(x<0))*1
+    x1, y1 = (x - 1), (y - 1)
+    x2, y2 = (x + 1), (y + 1)
+    canvas.create_oval(x1, y1, x2, y2, fill="black")
+
+
+def sgn(x):
+    return ((x > 0) - (x < 0)) * 1
+
 
 def processing(canvas, a, b, n):
-    na = 2 / n 
-    # defining the accuracy 
-    step = 10000
-    piece =(pi * 2)/step 
-    xp =[]
-    yp =[]
+    na = 2 / n
+    # defining the accuracy
+    step = 1000
+    piece = (pi * 2) / step
+    xp = []
+    yp = []
     t = 0
-    
-    for t1 in range(step + 1): 
-        # because sin ^ n(x) is mathematically the same as (sin(x))^n... 
-        x =(abs((cos(t)))**na)*a * sgn(cos(t)) 
-        y =(abs((sin(t)))**na)*b * sgn(sin(t)) 
+    for t1 in range(step + 1):
+        # because sin ^ n(x) is mathematically the same as (sin(x))^n...
+        x = (abs((cos(t))) ** na) * a * sgn(cos(t))
+        y = (abs((sin(t))) ** na) * b * sgn(sin(t))
         xp.append(x)
-        yp.append(y) 
-        t+= piece 
+        yp.append(y)
+        t += piece
 
     if len(xp) == len(yp):
         print("Точки совпадают")
         for i in range(len(xp)):
-            drawer(canvas, xp[i], yp[i])
+            drawer(canvas, xp[i], yp[i], a, b)
+            # drawer(canvas, xp[i], yp[i])
+
     else:
         print("Точки не совпадают")
-    #plt.plot(xp, yp) # plotting all point from array xp, yp 
+    # plt.plot(xp, yp) # plotting all point from array xp, yp
+
 
 if __name__ == "__main__":
 
@@ -41,18 +51,15 @@ if __name__ == "__main__":
     center_x = width // 2
     center_y = height // 2
 
-
     root = tk.Tk()
     c = tk.Canvas(root, width=width, heigh=height)
-    
 
     n = 0.4
-    while n != 2.5:    
-        a, b = 200, 200
-        processing(c,a,b,n)
-        n  = round(n + 0.1, 1)
+    while n != 2.5:
+        a, b = center_x, center_y
+        processing(c, a, b, n)
+        n = round(n + 0.1, 1)
         break
-
     c.pack()
 
     root.mainloop()
