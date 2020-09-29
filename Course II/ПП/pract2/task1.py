a49ddf234d63127fdbd6a450be5efa3998ecd9a4
Georgiy Demo <demenchuk.george@protonmail.com>
2020-09-29 10:47:45 +0300

    ПП #2 - готова #55

--- Course II/ПП/pract2/task1.py
+++ Course II/ПП/pract2/task1.py
@@ -1,84 +1,103 @@
+"""
+Практика 2 Суперэллипс
+Деменчук Георгий ПИ19-4
+"""
+
 import tkinter as tk
 from math import sin, cos, pi
+
+# Начальные позиции
+width = 600
+height = 600
+center_x = width // 2
+center_y = height // 2
+root = tk.Tk()
+# Основной канвас
+c = tk.Canvas(root, width=width, heigh=height)
+# Текущие точки, которые отрисованы
+points_list = []
 
 
 def drawer(canvas, x, y, a, b):
     """Создание точки"""
-    # Смещение
+
+    # Смещение, чтоб относительно центра
     x = a + x
     y = b + y
 
     x1, y1 = (x - 1), (y - 1)
     x2, y2 = (x + 1), (y + 1)
-    canvas.create_oval(x1, y1, x2, y2, fill="black")
+
+    point = canvas.create_oval(x1, y1, x2, y2, fill="white", outline="white")
+
+    # Добавляем точку в список точек
+    points_list.append(point)
 
 
-def sgn(x):
+def sign(x):
+    """Отдача знака для processing"""
     return ((x > 0) - (x < 0)) * 1
 
 
-def processing(canvas, a, b, n):
+def processing(canvas, n):
     """Метод отрисовки суперэллипса"""
+
+    a, b = center_x, center_y
     na = 2 / n
-    # defining the accuracy
+
+    # шаг отрисовки точек-овалов. если лагает - исправить на меньший коэфф
     step = 1000
     piece = (pi * 2) / step
     xp = []
     yp = []
+
     t = 0
     for t1 in range(step + 1):
-        # because sin ^ n(x) is mathematically the same as (sin(x))^n...
-        x = (abs((cos(t))) ** na) * a * sgn(cos(t))
-        y = (abs((sin(t))) ** na) * b * sgn(sin(t))
+
+        # т.к sin ^ n(x)  математически это то же самое, что (sin(x))^n...
+        x = (abs((cos(t))) ** na) * a * sign(cos(t))
+        y = (abs((sin(t))) ** na) * b * sign(sin(t))
         xp.append(x)
         yp.append(y)
         t += piece
 
     if len(xp) == len(yp):
-        print("Точки совпадают")
         for i in range(len(xp)):
             drawer(canvas, xp[i], yp[i], a, b)
-            # drawer(canvas, xp[i], yp[i])
-
     else:
-        print("Точки не совпадают")
+        raise ValueError("Точки x и y не совпадают")
 
-#TODO
-def onScale(number):
+
+def scale_processing(number):
     """Обработка ползунка"""
-    print(number)
-    #c - canvas
-    #a, b = center_x, center_y
 
-    #processing(c, a, b, n)
+    # Удаляем предыдущие точки, если они есть
+    if len(points_list) != 0:
+        for point in points_list:
+            c.delete(point)
+
+    # Отрисовываем эллипс
+    processing(c, float(number))
 
 
 def main():
 
-    width = 600
-    height = 600
-    center_x = width // 2
-    center_y = height // 2
+    root.title("Суперэллипс")
 
-
-    root = tk.Tk()
-    #Основной канвас
-    c = tk.Canvas(root, width=width, heigh=height)
-    
-    #Начальное значение n
-    n = 0.4
-    while n != 2.5:
-        #
-        a, b = center_x, center_y
-
-        processing(c, a, b, n)
-        n = round(n + 0.1, 1)
-        break
-    
-    #Распаковка канваса
+    # Распаковка канваса
+    c.configure(bg="black")
     c.pack(fill=tk.BOTH, expand=1)
-    #Распаковка ползунка
-    scale = tk.Scale(root, from_=0.35, to=2.75, digits = 3, resolution = 0.01, command=onScale, orient=tk.HORIZONTAL)
+
+    # Распаковка ползунка
+    scale = tk.Scale(
+        root,
+        from_=0.01,
+        to=3.75,
+        digits=3,
+        resolution=0.01,
+        command=scale_processing,
+        orient=tk.HORIZONTAL,
+    )
     scale.pack(side=tk.LEFT, padx=5)
 
     root.mainloop()
