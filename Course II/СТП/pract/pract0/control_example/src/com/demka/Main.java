46d3a023392e1b7fa0d41c91dcde81697d5930f2
Georgiy Demo <demenchuk.george@protonmail.com>
2020-10-05 16:07:17 +0300

    СТП - пример контрольной (начало)

---
+++ Course II/СТП/pract/pract0/control_example/src/com/demka/Main.java
@@ -0,0 +1,23 @@
+package com.demka;
+
+/*
+Класс автомобиля
+ */
+class Car {
+
+}
+
+/*
+Класс гаража, где хранятся автомобили
+ */
+class Garage {
+
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
