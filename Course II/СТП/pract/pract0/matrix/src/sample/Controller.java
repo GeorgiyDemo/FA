6cb34defec4b07581143d4d2d4fe5146b73d8700
Georgiy Demo <demenchuk.george@protonmail.com>
2020-10-08 09:45:13 +0300

    Matrix - fxml #60

--- Course II/СТП/pract/pract0/matrix/src/sample/Controller.java
+++ Course II/СТП/pract/pract0/matrix/src/sample/Controller.java
@@ -10,24 +10,6 @@ import javafx.scene.control.TextField;
 public class Controller {
 
     @FXML
-    private TextField vectorATextField;
-    @FXML
-    private TextField vectorBTextField;
-    @FXML
-    private Label lengthALabel;
-    @FXML
-    private  Label lengthBLabel;
-    @FXML
-    private Label dotProductLabel;
-    @FXML
-    private Label crossProductLabel;
-    @FXML
-    private Label vectorCosLabel;
-    @FXML
-    private Label summLabel;
-    @FXML
-    private Label diffLabel;
-    @FXML
     private Label wrongInputLabel;
 
 
@@ -36,35 +18,7 @@ public class Controller {
      */
     @FXML
     private void mainButtonClick() {
-        String vectorAString = vectorATextField.getText();
-        String vectorBString = vectorBTextField.getText();
-        FilterInput filter = new FilterInput(vectorAString, vectorBString);
-
-        //Если успешно прошли валидацию данных
-        if (filter.Validate()){
-
-            wrongInputLabel.setVisible(false);
-            int [] buf1 = filter.getV1result();
-            int [] buf2 = filter.getV2result();
-
-            Vector vectorA = new Vector(buf1[0], buf1[1], buf1[2]);
-            Vector vectorB = new Vector(buf2[0], buf2[1], buf2[2]);
-
-            lengthALabel.setText("Длина вектора А:\n" + vectorA.length());
-            lengthBLabel.setText("Длина вектора B:\n" + vectorB.length());
-
-            dotProductLabel.setText("Скалярное произведение вектора A и B:\n" + vectorA.DotProduct(vectorB));
-            crossProductLabel.setText("Векторное произведение вектора A и B:\n"+vectorA.CrossProduct(vectorB).value());
-            vectorCosLabel.setText("Угол между векторами A и B:\n" + vectorA.VectorCos(vectorB));
-
-            summLabel.setText("Сумма векторов A и B:\n"+vectorA.summ(vectorB).value());
-            diffLabel.setText("Разность векторов A и B:\n"+vectorA.difference(vectorB).value());
-
-        }
-        else {
-            wrongInputLabel.setVisible(true);
-        }
-
+        System.out.println("WORKING");
 
     }
 }
--- Course II/СТП/pract/pract0/matrix/src/sample/Main.java
+++ Course II/СТП/pract/pract0/matrix/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1250, 700));
         primaryStage.show();
     }
 
