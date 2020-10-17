bf88efa31cd93fc405b539e767cc484dc67eddf2
Georgiy Demo <demenchuk.george@protonmail.com>
2020-10-17 17:14:42 +0300

    R- проверить на адекватность работу экспорта .csv

--- Course II/R/Diksi/code/task2.r
+++ Course II/R/Diksi/code/task2.r
@@ -3,53 +3,91 @@
 library("xlsx")
 
 #Цена продукта, который мы продаём
-PRODUCT_PRICE = 500
+PRODUCT_PRICE <- 500
+#цена поставки
+SUPPLY_PRICE <- 65
+UNTIL_PRICE <- 30
+
 #Устанавливаем директорию
 setwd("/Users/georgiydemo/Projects/FA/Course II/R/Diksi/analytics")
 
 #Названия магазинов
 shop_names <- c()
 #Выручка
-shop_revenues <- rep(0, 10)
+shop_revenues <- c()
 #Прибыль
-shop_profits <- rep(0, 10)
+shop_profits <- c()
 #Реализация
 shop_sales <- c()
 #Списание
 shop_writeoffs <- c()
 #Равномерность продаж
-shop_sr <- rep(0, 10)
+shop_sr <- c()
 #Продажи макс
-shop_sales_max <- rep(0, 10)
+shop_sales_max <- c()
 #День продажи макс
-shop_sales_maxdays <- rep(0, 10)
+shop_sales_maxdays <- c()
 #Продажи мин
-shop_sales_min <- rep(0, 10)
+shop_sales_min <- c()
 #День продажи мин
-shop_sales_mindays <- rep(0, 10)
+shop_sales_mindays <- c()
 #Списание макс
-shop_writeoff_max <- rep(0, 10)
+shop_writeoff_max <- c()
 #День списания макс 
-shop_writeoff_maxdays <- rep(0, 10)
+shop_writeoff_maxdays <- c()
 
 
+#Цикл по каждому магазину
 for (i in 1:10){
+  
   in1 <- read.table(file = paste0('store',as.character(i),'_in.txt'), head = TRUE)
   out1 <- read.table(file = paste0('store',as.character(i),'_out.txt'), head = TRUE)
   
-  #Название магазина
+  # Название магазина
   shop_names <- append(shop_names, paste0('store',as.character(i)))
   
-  #TODO Прибыль
+  # Списание
+  buf_writeoff <- sum(in1[, 2]) - sum(out1[, 2])
+  shop_writeoffs <- append(shop_writeoffs, buf_writeoff)
   
-  #Реализация
+  # Выручка
+  buf_shoprevenue <- PRODUCT_PRICE * sum(out1[, 2])
+  shop_revenues <- append(shop_revenues, buf_shoprevenue)
+  
+  # Затраты
+  buf_cost <- (sum(in1[, 2]) * SUPPLY_PRICE) + (buf_writeoff * UNTIL_PRICE)
+  
+  # Прибыль
+  shop_profits <- append(shop_profits, buf_shoprevenue - buf_cost)
+  
+  # Реализация
   shop_sales <- append(shop_sales, sum(out1[, 2]))
   
-  #Списание
-  shop_writeoffs <- append(shop_writeoffs, sum(in1[, 2]) - sum(out1[, 2]))
+  # Равномерность продаж
+  shop_sr <- append(shop_sr, sd(out1[, 2]))
+  
+  # Продажи макс
+  shop_sales_max <-append(shop_sales_max, max(out1[, 2]))
+
+  # День продажи макс
+  shop_sales_maxdays <- append(shop_sales_maxdays, out1[which.max(out1[, 2]), 1])
+  
+  # Продажи мин
+  shop_sales_min <-append(shop_sales_min, min(out1[, 2]))
+  
+  # День продажи мин
+  shop_sales_mindays <- append(shop_sales_mindays, out1[which.min(out1[, 2]), 1])
+  
+  # Списание макс
+  shop_writeoff_max <- append(shop_writeoff_max,c(in1[, 2] - out1[, 2]))
+
+  # День списания макс
+  shop_writeoff_maxdays <- append(shop_writeoff_maxdays, in1[which.max(c(in1[, 2] - out1[, 2])), 1])
+  
+  print("ВЫПОЛНИЛИСЬ N РАЗ")
+  print(i)
 }
 
-print(shop_names)
 
 #Формируем датафрейм
 table <- data.frame(shop_names,shop_revenues,shop_profits,shop_sales ,shop_writeoffs,shop_sr,shop_sales_max,shop_sales_maxdays,shop_sales_min,shop_sales_mindays,shop_writeoff_max,shop_writeoff_maxdays)
