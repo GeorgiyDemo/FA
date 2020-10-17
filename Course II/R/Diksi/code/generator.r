7f562421733eb584c037a011df03bcf90b8276f4
Georgiy Demo <demenchuk.george@protonmail.com>
2020-10-17 13:19:16 +0300

    R - обновили основной генератор

--- Course II/R/Diksi/code/generator.r
+++ Course II/R/Diksi/code/generator.r
@@ -1,110 +1,82 @@
-saleLevel <- sample(0:100, 10, replace = T)
-all.in <- vector()
-all.out <- vector()
+#Максимальное число попыток, чтоб сгенерировать данные
+MAX_TRY <- 10000
+#Уровень продаж
+SALE_LEVEL <- 85
 
-#Заполняем поставки
-for (i in (10:20)) {
-  all.in <- c(all.in, round(runif(7) * 30 * i + 10 * i))
+#Генерация поставок
+generate.in <- function(nDays = 7, min = 50, max= 120){
+  return(as.integer(runif(n=nDays, min=min,max=max)))
 }
 
-a <-
-  c("Понедельник",
-    "Вторник",
-    "Среда",
-    "Четверг",
-    "Пятница",
-    "Суббота",
-    "Воскресенье")
-in.tab1 <- data.frame("День недели" = a, "Поставки" = c(all.in[1:7]))
-in.tab2 <- data.frame("День недели" = a, "Поставки" = c(all.in[8:14]))
-in.tab3 <- data.frame("День недели" = a, "Поставки" = c(all.in[15:21]))
-in.tab4 <- data.frame("День недели" = a, "Поставки" = c(all.in[22:28]))
-in.tab5 <- data.frame("День недели" = a, "Поставки" = c(all.in[29:35]))
-in.tab6 <- data.frame("День недели" = a, "Поставки" = c(all.in[36:42]))
-in.tab7 <- data.frame("День недели" = a, "Поставки" = c(all.in[43:49]))
-in.tab8 <- data.frame("День недели" = a, "Поставки" = c(all.in[50:56]))
-in.tab9 <- data.frame("День недели" = a, "Поставки" = c(all.in[57:63]))
-in.tab10 <- data.frame("День недели" = a, "Поставки" = c(all.in[64:70]))
-
-#Заполняем продажу с зависимостью от уровня поставок и поставок
-for (i in (1:length(all.in))) {
-  all.out <-
-    append(all.out, round(all.in[i] * saleLevel[((i - 1) %/% 7)] / 100))
+#Генерация продаж
+generate.out <- function(data.in, saleLevel = 50){
+  
+  sum.in <- sum(data.in)
+  n <- 0 #счетчик цикла
+  repeat{
+    n<-n+1
+    
+    data.out <- 0
+    for(i in 1:length(data.in)){
+      data.out[i] <- as.integer(runif(n=1,min=0,max=data.in[i]))
+    }
+    
+    sum.out <- sum(data.out)
+    ratio <- sum.out /sum.in * 100
+    
+    if ((ratio <= saleLevel * 1.005) && (ratio >= saleLevel * 0.995) || (n == MAX_TRY)){
+      break
+    }
+  }
+  
+  return(data.out)
 }
 
-out.tab1 <- data.frame("День недели" = a, "Поставки" = c(all.out[1:7]))
-out.tab2 <- data.frame("День недели" = a, "Поставки" = c(all.out[8:14]))
-out.tab3 <- data.frame("День недели" = a, "Поставки" = c(all.out[15:21]))
-out.tab4 <- data.frame("День недели" = a, "Поставки" = c(all.out[22:28]))
-out.tab5 <- data.frame("День недели" = a, "Поставки" = c(all.out[29:35]))
-out.tab6 <- data.frame("День недели" = a, "Поставки" = c(all.out[36:42]))
-out.tab7 <- data.frame("День недели" = a, "Поставки" = c(all.out[43:49]))
-out.tab8 <- data.frame("День недели" = a, "Поставки" = c(all.out[50:56]))
-out.tab9 <- data.frame("День недели" = a, "Поставки" = c(all.out[57:63]))
-out.tab10 <-
-  data.frame("День недели" = a, "Поставки" = c(all.out[64:70]))
 
-#Если продаем больше, чем покупаем, то это неправильно и исправаляем это недоразумение
-if (sum(in.tab1[, 2]) < sum(out.tab1[, 2])) {
-  t <- out.tab1[, 2]
-  out.tab1[, 2] <- in.tab1[, 2]
-  in.tab1 <- t
+{
+  days <- c("Понедельник", "Вторник","Среда","Четверг","Пятница","Суббота","Воскресенье")
+  
+  #Цикл по каждому магазину
+  for (i in 1:10){
+    
+    #генерируем поставки
+    dataIn <- generate.in(nDays=7,min=40, max=150)
+    
+    #генерируем продажи
+    dataOut <-generate.out(dataIn,saleLevel = SALE_LEVEL)
+    
+    #Закидываем во фрейм поставок
+    in.tabl <- data.frame("День недели" = a, "Поставки" = dataIn)
+    
+    #Закидываем во фрейм продаж
+    out.tabl <- data.frame("День недели" = a, "Поставки" = dataOut)
+    
+    #Записываем файл поставок
+    write.table(
+      in.tabl,
+      file = paste('/Users/georgiydemo/Projects/FA/Course II/R/Diksi/shop',as.character(i),'/in.txt',sep = ''),
+      col.names = TRUE,
+      row.names = FALSE,
+      sep = ' ',
+      dec = ','
+    )
+    
+    #Записываем файл продаж
+    write.table(
+      out.tabl,
+      file = paste(
+        '/Users/georgiydemo/Projects/FA/Course II/R/Diksi/shop',
+        as.character(i),
+        '/out.txt',
+        sep = ''
+      ),
+      col.names = TRUE,
+      row.names = FALSE,
+      sep = ' ',
+      dec = ','
+    )
+    
+    print(paste("Генерация данных для магазина",i,"успешна"))
+  }
 }
 
-#Закидываем все значения in в вектор
-in.tabl <-
-  c(
-    in.tab1,
-    in.tab2,
-    in.tab3,
-    in.tab4,
-    in.tab5,
-    in.tab6,
-    in.tab7,
-    in.tab8,
-    in.tab9,
-    in.tab10
-  )
-#Закидываем все значения out в вектор
-out.tabl <-
-  c(
-    out.tab1,
-    out.tab2,
-    out.tab3,
-    out.tab4,
-    out.tab5,
-    out.tab6,
-    out.tab7,
-    out.tab8,
-    out.tab9,
-    out.tab10
-  )
-
-for (i in (1:10)) {
-  write.table(
-    in.tabl[c(i * 2 - 1, i * 2)],
-    file = paste(
-      '/Users/georgiydemo/Projects/FA/Course II/R/Diksi/shop',
-      as.character(i),
-      '/in.txt',
-      sep = ''
-    ),
-    col.names = TRUE,
-    row.names = FALSE,
-    sep = ' ',
-    dec = ','
-  )
-  write.table(
-    out.tabl[c(i * 2 - 1, i * 2)],
-    file = paste(
-      '/Users/georgiydemo/Projects/FA/Course II/R/Diksi/shop',
-      as.character(i),
-      '/out.txt',
-      sep = ''
-    ),
-    col.names = TRUE,
-    row.names = FALSE,
-    sep = ' ',
-    dec = ','
-  )
-}
