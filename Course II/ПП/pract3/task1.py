7a5d461fa90fcaecfd9f076b7c52333494c9145f
Georgiy Demo <demenchuk.george@protonmail.com>
2020-10-21 13:44:31 +0300

    ПП 3 - прототип #69

--- Course II/ПП/pract3/task1.py
+++ Course II/ПП/pract3/task1.py
@@ -8,6 +8,7 @@
 """
 
 import tkinter as tk
+import time
 from math import sin, cos, pi
 
 # Начальные позиции
@@ -16,94 +17,71 @@ height = 600
 root = tk.Tk()
 # Основной канвас
 c = tk.Canvas(root, width=width, heigh=height)
-# Текущие точки, которые отрисованы
-points_list = []
 
+class PointF:
+    """Класс точки"""
+    def __init__(self, x, y):
+        self.X = x
+        self.Y = y
 
-def drawer(canvas, x, y, a, b):
-    """Создание точки"""
+def Draw():
 
-    # Смещение, чтоб относительно центра
-    x = a + x
-    y = b + y
+    #Определим коорднаты исходного треугольника
+    point1 = PointF(200, 200)
+    point2 = PointF(500, 200)
+    point3 = PointF(350, 400)
 
-    x1, y1 = (x - 1), (y - 1)
-    x2, y2 = (x + 1), (y + 1)
+    #Вызываем функцию Fractal для того, чтобы
+    #нарисовать три кривых Коха на сторонах треугольника
+    Fractal(point1, point2, point3, 5)
+    Fractal(point2, point3, point1, 5)
+    Fractal(point3, point1, point2, 5)
 
-    point = canvas.create_oval(x1, y1, x2, y2, fill="white", outline="white")
+def Fractal(p1, p2, p3, iter):
 
-    # Добавляем точку в список точек
-    points_list.append(point)
+    if iter > 0:
+        p4 = PointF((p2.X + 2 * p1.X) / 3, (p2.Y + 2 * p1.Y) / 3)
+        p5 = PointF((2 * p2.X + p1.X) / 3, (p1.Y + 2 * p2.Y) / 3)
+        
+        #координаты вершины угла
+        ps = PointF((p2.X + p1.X) / 2, (p2.Y + p1.Y) / 2)
+        pn = PointF((4 * ps.X - p3.X) / 3, (4 * ps.Y - p3.Y) / 3)
+        
+        #рисуем его
 
+        c.create_line(p4.X,p4.Y, pn.X, pn.Y)
+        c.create_line(p5.X,p5.Y, pn.X, pn.Y)
+        c.create_line(p4.X,p4.Y, p5.X, p5.Y)
+        
+        #g.DrawLine(pen1, p4, pn)
+        #g.DrawLine(pen1, p5, pn)
+        #g.DrawLine(pen2, p4, p5)
+        
+        #рекурсивно вызываем функцию нужное число раз
+        Fractal(p4, pn, p5, iter - 1)
+        Fractal(pn, p5, p4, iter - 1)
+        Fractal(p1, p4, PointF((2 * p1.X + p3.X) / 3, (2 * p1.Y + p3.Y) / 3), iter - 1)
+        Fractal(p5, p2, PointF((2 * p2.X + p3.X) / 3, (2 * p2.Y + p3.Y) / 3), iter - 1)
 
-def sign(x):
-    """Отдача знака для processing"""
-    return ((x > 0) - (x < 0)) * 1
-
-
-def processing(canvas, n):
-    """Метод отрисовки суперэллипса"""
-
-    a, b = width // 2, height // 2
-    na = 2 / n
-
-    # шаг отрисовки точек-овалов. если лагает - исправить на меньший коэфф
-    step = 1000
-    piece = (pi * 2) / step
-    xp = []
-    yp = []
-
-    t = 0
-    for _ in range(step + 1):
-
-        # т.к sin ^ n(x)  математически это то же самое, что (sin(x))^n...
-        x = (abs((cos(t))) ** na) * a * sign(cos(t))
-        y = (abs((sin(t))) ** na) * b * sign(sin(t))
-        xp.append(x)
-        yp.append(y)
-        t += piece
-
-    if len(xp) == len(yp):
-        for i in range(len(xp)):
-            drawer(canvas, xp[i], yp[i], a, b)
     else:
-        raise ValueError("Точки x и y не совпадают")
+        return iter
 
 
-def scale_processing(number):
-    """Обработка ползунка"""
 
-    # Удаляем предыдущие точки, если они есть
-    if len(points_list) != 0:
-        for point in points_list:
-            c.delete(point)
-
-    # Отрисовываем эллипс
-    processing(c, float(number))
 
 
 def main():
 
-    root.title("Суперэллипс")
+    root.title("Снежинка Коха")
 
     # Распаковка канваса
-    c.configure(bg="black")
+    c.configure(bg="white")
     c.pack(fill=tk.BOTH, expand=1)
 
-    # Распаковка ползунка
-    scale = tk.Scale(
-        root,
-        from_=0.01,
-        to=3.75,
-        digits=3,
-        resolution=0.01,
-        command=scale_processing,
-        orient=tk.HORIZONTAL,
-    )
-    scale.pack(side=tk.LEFT, padx=5)
-
+    Draw()
     root.mainloop()
 
 
 if __name__ == "__main__":
     main()
+
