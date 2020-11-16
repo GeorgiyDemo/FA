98706ec33609a919634120b0309dd2e1770f7d6d
Georgiy Demo <demenchuk.george@protonmail.com>
2020-11-16 13:53:44 +0300

    СТП - 3 практ начало #82

---
+++ Course II/СТП/pract/pract3/src/com/demka/Main.java
@@ -0,0 +1,16 @@
+//Работа с потоками в Java
+
+package com.demka;
+
+//Наследование от Thread
+
+
+
+//С помощью Runable
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("MEOW");
+	// write your code here
+    }
+}
