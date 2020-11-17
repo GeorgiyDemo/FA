30f55c038669f29ed0d6dd4fac770a1b3be1a3b6
Georgiy Demo <demenchuk.george@protonmail.com>
2020-11-17 12:49:14 +0300

    СТП - чтение из файла

--- Course II/СТП/pract/examples/FileWriter/src/com/demka/Main.java
+++ Course II/СТП/pract/examples/FileWriter/src/com/demka/Main.java
@@ -6,10 +6,13 @@
 
 package com.demka;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 class DemkaWriter {
     int AllSeconds;
@@ -23,8 +26,12 @@ class DemkaWriter {
         this.processing();
     }
 
+    public String getFilename() {
+        return filename;
+    }
+
     void processing() {
-        System.out.println("Открыли файл" + this.filename + "на запись");
+        System.out.println("Открыли файл" + this.filename + " на запись");
         try (FileWriter writer = new FileWriter(this.filename, false)) {
             while (AllSeconds > 0) {
                 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
@@ -40,11 +47,44 @@ class DemkaWriter {
             System.out.println(ex.getMessage());
         }
     }
+
 }
+
+
+class DemkaReader{
+    String filename;
+
+    public DemkaReader(String filename){
+        this.filename = filename;
+        System.out.println("Чтение из файла..");
+        this.processing();
+    }
+
+    public void processing(){
+
+        try {
+            File myObj = new File(this.filename);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Ошибка, файла нет");
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
+
 
 public class Main {
 
     public static void main(String[] args) {
         DemkaWriter obj = new DemkaWriter(100);
+        DemkaReader obj1 = new DemkaReader(obj.getFilename());
     }
 }
