219902532671b09d6daf242b87daea8a95b66e77
Georgiy Demo <demenchuk.george@protonmail.com>
2020-11-17 11:37:18 +0300

    СТП - начало #82

--- Course II/СТП/pract/pract3/src/com/demka/Main.java
+++ Course II/СТП/pract/pract3/src/com/demka/Main.java
@@ -3,14 +3,48 @@
 package com.demka;
 
 //Наследование от Thread
+class MyThread extends Thread {
+
+    String value;
+    int delay;
+
+    public MyThread(String value, int delay){
+        this.value = value;
+        this.delay = delay;
+
+    }
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            try {
+                Thread.sleep(delay);
+                System.out.println(this.value);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+}
+
 
 
 
 //С помощью Runable
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("MEOW");
-	// write your code here
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new MyThread("1111",1000);
+        Thread thread2 = new MyThread("2222",500);
+        thread1.start();
+        thread2.start();
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("ПРИВЕТ ОТ MAIN");
+            Thread.sleep(500);
+        }
+
     }
 }
