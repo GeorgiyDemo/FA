b44bd3c1ed3324af30b58103faa21a44ecdc9901
Georgiy Demo <demenchuk.george@protonmail.com>
2020-11-26 16:30:14 +0300

    UNIX -  shell через цикл #88

--- Course II/UNIX/pract/LX 2.4 Обработка текста /shell.sh
+++ Course II/UNIX/pract/LX 2.4 Обработка текста /shell.sh
@@ -6,26 +6,6 @@ ps -aux > ps.out
 #Отсортированный список процессов по имени пользователя-владельца
 sort ps.out > sorted.ps
 
-#TODO ЦИКЛ ПО КАЖДОМУ ПОЛЬЗОВАТЕЛЮ
-DATA=$(cat sorted.ps)
-for user in $DATA
-    do
-        OUT=$(cut -d " " -f 1)
-        echo $OUT
-
-    done
-
-cat sorted.ps | grep root > root.ps
-cat sorted.ps | grep $(whoami) > user.ps
-
-#Добавление тега <li>
-sed -e "s/root/<li> root/g" root.ps > root.html
-sed -e "s/$/<li> $/g" user.ps > user.html
-
-#Подсчет колличества строк
-echo "<p><b>Итого процессов: $(cat root.html | wc -l) </b></p>" > root.total
-echo "<p><b>Итого процессов: $(cat user.html | wc -l) </b></p>" > user.total
-
 #Создание файла index.html
 splitter='<hr noshade>'
 body='<html><head><meta charset="utf-8"><title>Статистика процессов</title></head><body><h1>Распределение процессов по пользователям</h1><div class="row"><div class="col-12">'
@@ -35,4 +15,5 @@ echo $body > index.html
 #Создание файла index.html
 echo $(cat index.html) $splitter $(cat root.total) "Пользователь root" $splitter $(cat root.html) $splitter $(cat user.total) "Пользователь" $(whoami) $splitter $(cat user.html) $footer > index.html
 
+echo $footer >> index.html
 echo "Завершение работы скрипта"
