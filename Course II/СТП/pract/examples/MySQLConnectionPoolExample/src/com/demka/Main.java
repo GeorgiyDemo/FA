defdb581a12a09d66a2edbbc4be1b6349ec6b9c2
Georgiy Demo <demenchuk.george@protonmail.com>
2020-12-08 13:22:44 +0300

    СТП Java - Запись данных в БД

--- Course II/СТП/pract/examples/MySQLConnectionPoolExample/src/com/demka/Main.java
+++ Course II/СТП/pract/examples/MySQLConnectionPoolExample/src/com/demka/Main.java
@@ -10,20 +10,29 @@ public class Main {
    public static void main(String[] args) throws SQLException, SQLException {
         Connection conn = null;
         MySQLConnectionPool pool = new MySQLConnectionPool(
-                "jdbc:mysql://localhost:3306/JavaDB",
+                "jdbc:mysql://localhost:3306/JavaDB?characterEncoding=utf-8",
                 "root", "tiger", 2);
         try {
             conn = pool.getConnection();
             try (Statement statement = conn.createStatement())
             {
                 ResultSet res = statement.executeQuery("show tables");
-                System.out.println("There are below tables:");
+                System.out.println("Таблицы в БД:");
+
                 while (res.next()) {
                     String tblName = res.getString(1);
                     System.out.println(tblName);
                 }
+
+                String SQLString = "INSERT INTO students (first_name,last_name) VALUES('ИМЯ ИМЯ ИМЯ','ФАМИЛИЯ ФАМИЛИЯ ФАМИЛИЯ');";
+                boolean resultFlag = statement.execute(SQLString);
+                System.out.println("Результат: "+resultFlag);
+
             }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
+
         finally {
             if (conn != null) {
                 pool.returnConnection(conn);
