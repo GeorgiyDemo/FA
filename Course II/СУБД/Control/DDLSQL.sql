d2df10aa39a391875b6c7b3cc6fa239eb0a1d23b
Georgiy Demo <demenchuk.george@protonmail.com>
2020-12-24 11:42:18 +0300

    СУБД фиксы #99

--- Course II/СУБД/Control/DDLSQL.sql
+++ Course II/СУБД/Control/DDLSQL.sql
@@ -9,11 +9,11 @@ DROP TABLE products;
 
 CREATE TABLE clients (
     id                 NUMBER,
-    first_name         VARCHAR2(400) NOT NULL,
-    last_name          VARCHAR2(400) NOT NULL,
-    email              VARCHAR2(400) NOT NULL,
-    phone              VARCHAR2(400) NOT NULL,
-    document_title     VARCHAR2(400) NOT NULL,
+    first_name         VARCHAR2(40) NOT NULL,
+    last_name          VARCHAR2(40) NOT NULL,
+    email              VARCHAR2(40) NOT NULL,
+    phone              VARCHAR2(40) NOT NULL,
+    document_title     VARCHAR2(40) NOT NULL,
     document_file      RAW(2000) NOT NULL,
     document_text      VARCHAR2(400) NOT NULL,
     document_comments  VARCHAR2(400),
@@ -23,11 +23,11 @@ CREATE TABLE clients (
 
 CREATE TABLE staffs (
     id          NUMBER,
-    first_name  VARCHAR2(400) NOT NULL,
-    last_name   VARCHAR2(400) NOT NULL,
-    position    VARCHAR2(400) NOT NULL,
-    type        VARCHAR2(400) NOT NULL,
-    phone       VARCHAR2(400) NOT NULL,
+    first_name  VARCHAR2(40) NOT NULL,
+    last_name   VARCHAR2(40) NOT NULL,
+    position    VARCHAR2(40) NOT NULL,
+    type        VARCHAR2(40) NOT NULL,
+    phone       VARCHAR2(40) NOT NULL,
     CONSTRAINT staff_pk PRIMARY KEY ( id )
 );
 
@@ -45,32 +45,30 @@ CREATE TABLE bookings (
 
 CREATE TABLE houses (
     id           NUMBER,
-    name         VARCHAR2(400) NOT NULL,
+    name         VARCHAR2(40) NOT NULL,
     price        FLOAT NOT NULL,
     ac           NUMBER NOT NULL,
     tv           NUMBER NOT NULL,
     safe         NUMBER NOT NULL,
     description  VARCHAR2(400),
-    type         VARCHAR2(400),
+    type         VARCHAR2(40),
     booking_id   NUMBER,
     CONSTRAINT house_pk PRIMARY KEY ( id ),
     CONSTRAINT house_booking_fk FOREIGN KEY ( booking_id ) REFERENCES bookings ( id )
 );
 
-
-
 CREATE TABLE orders (
-    id         NUMBER,
-    "date"     DATE NOT NULL,
-    client_id  NUMBER NOT NULL,
-    cost       FLOAT NOT NULL,
+    id          NUMBER,
+    order_date  DATE NOT NULL,
+    client_id   NUMBER NOT NULL,
+    cost        FLOAT NOT NULL,
     CONSTRAINT order_pk PRIMARY KEY ( id ),
     CONSTRAINT order_client_fk FOREIGN KEY ( client_id ) REFERENCES clients ( id )
 );
 
 CREATE TABLE products (
     id     NUMBER,
-    title  VARCHAR2(400) NOT NULL,
+    title  VARCHAR2(40) NOT NULL,
     price  FLOAT NOT NULL,
     CONSTRAINT product_pk PRIMARY KEY ( id )
 );
@@ -83,7 +81,6 @@ CREATE TABLE products_count (
     CONSTRAINT product_count_order_fk FOREIGN KEY ( order_id ) REFERENCES orders ( id ),
     CONSTRAINT product_count_product_fk FOREIGN KEY ( product_id ) REFERENCES products ( id )
 );
-
 
 CREATE TABLE staffs_houses (
     house_id  NUMBER NOT NULL,
