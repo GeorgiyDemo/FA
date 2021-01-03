7a09adcaed936427528d73bfc05b5bd1b1765852
Demka <GeorgiyDemo@users.noreply.github.com>
2021-01-03 19:39:54 +0300

    UNIX task4 ready #104

--- Course II/UNIX/exam/task4.sh
+++ Course II/UNIX/exam/task4.sh
@@ -1,3 +1,11 @@
 : '
 Написать скрипт, который выведет всех потомков процесса по его PID. 
 '
+#Имя процесса
+PROCNAME=$(ps -P $1 | tail -1 | awk '{ print $6 }')
+echo "Потомки процесса $PROCNAME с PID $1:"
+
+# Отображаем информацию о каждом процессе по PID, отданному в pgrep
+pgrep -P $1| while read pid; do
+    ps -p $pid | tail -1 | awk '{ print $1, $4 }'
+done
