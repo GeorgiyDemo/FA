cc7a80d450e0750db4ad4d91a736d29ad31820bd
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-02-19 15:11:26 +0300

    Начало генерации данных

--- Course II/Python_SQL/pract/pract3/control/control.py
+++ Course II/Python_SQL/pract/pract3/control/control.py
@@ -1,6 +1,7 @@
 import sqlalchemy
 from typing import List, Tuple
 from datetime import datetime
+from faker import Faker
 from sqlalchemy import (
     MetaData,
     Table,
@@ -18,23 +19,56 @@ from sqlalchemy.sql import select
 from sqlalchemy import PrimaryKeyConstraint, UniqueConstraint, CheckConstraint
 
 
-
 class Generator:
+    """Генерация данных для сущностей"""
+
+    #TODO
+    CUSTOMER_ID = 1
+
     def __init__(self):
-        pass
+        self.fake = Faker("ru_RU")
 
-    def gen_customer(self):
+    def gen_customer(self) -> List:
         """Генерация клиента"""
-        pass
+        result_list = []
 
+        # Текущий ID
+        customer_id = Generator.CUSTOMER_ID
+        Generator.CUSTOMER_ID += 1
+
+        last_name = "Иванов"
+        first_name = "Иван"
+        middle_name = "Иванович"
+        street = "Ул 8 марта"
+        city = self.fake.city()
+        state = self.fake.city()
+        zip = "4386438906"
+        phone = self.fake.phone_number()
+        email = self.fake.email()
+        return [
+            customer_id,
+            last_name,
+            first_name,
+            middle_name,
+            street,
+            city,
+            state,
+            zip,
+            phone,
+            email,
+        ]
+
+    #TODO
     def gen_account_types(self):
         """Генерация типов аккаунтов"""
         pass
 
+    #TODO
     def gen_transactions(self):
         """Генерация операций"""
         pass
 
+    #TODO
     def gen_customers_accounts(self, customer_id: int, account_id: int):
         """Генерация промежуточной таблицы"""
         pass
@@ -42,9 +76,12 @@ class Generator:
 
 class DatabaseProcessing:
     def __init__(self, sql_connection: str) -> None:
-        self.engine = create_engine("sqlite:///Demenchuk_bank.db")
+        engine = create_engine("sqlite:///Demenchuk_bank.db")
+        self.connection = engine.connect()
+        self.engine = engine
         self.metadata = MetaData()
-        #Словарь с хранением таблиц
+
+        # Словарь с хранением таблиц
         self.tables_dict = {}
 
         self.generate_table()
@@ -70,7 +107,18 @@ class DatabaseProcessing:
         )
         self.tables_dict["customers"] = {}
         self.tables_dict["customers"]["table"] = customers
-        self.tables_dict["customers"]["fields"] = ["customer_id", "last_name", "first_name", "middle_name", "street", "city", "state", "zip", "phone", "email"]
+        self.tables_dict["customers"]["fields"] = [
+            "customer_id",
+            "last_name",
+            "first_name",
+            "middle_name",
+            "street",
+            "city",
+            "state",
+            "zip",
+            "phone",
+            "email",
+        ]
 
         account_types = Table(
             "account_types",
@@ -98,7 +146,15 @@ class DatabaseProcessing:
         )
         self.tables_dict["accounts"] = {}
         self.tables_dict["accounts"]["table"] = accounts
-        self.tables_dict["accounts"]["fields"] = ["account_id", "type", "description", "balance", "credit_line","begin_balance", "begin_balance_timestamp"]
+        self.tables_dict["accounts"]["fields"] = [
+            "account_id",
+            "type",
+            "description",
+            "balance",
+            "credit_line",
+            "begin_balance",
+            "begin_balance_timestamp",
+        ]
 
         transactions = Table(
             "transactions",
@@ -113,7 +169,15 @@ class DatabaseProcessing:
         )
         self.tables_dict["transactions"] = {}
         self.tables_dict["transactions"]["table"] = accounts
-        self.tables_dict["transactions"]["fields"] = ["transaction_id", "account_id", "timestamp", "amount", "balance","begin_balance", "description"]
+        self.tables_dict["transactions"]["fields"] = [
+            "transaction_id",
+            "account_id",
+            "timestamp",
+            "amount",
+            "balance",
+            "begin_balance",
+            "description",
+        ]
 
         # Связь многие ко многим для таблиц customers и accounts
         customers_accounts = Table(
@@ -130,47 +194,50 @@ class DatabaseProcessing:
         # Создали все данные
         metadata.create_all(self.engine)
 
-    def insert(self, table_name : str, values : List) -> int:
+    def insert(self, table_name: str, values: List) -> int:
         """Вставка данных в таблицу table_name"""
 
         if table_name not in self.tables_dict:
             raise ValueError(f"Переданной таблицы {table_name} не существует!")
-        
+
         current_table = self.tables_dict[table_name]["table"]
         fields_list = self.tables_dict[table_name]["fields"]
 
         ins = current_table.insert().values(**dict(zip(fields_list, values)))
-        connection = self.engine.connect()
-        result = connection.execute(ins)
-        return result.inserted_primary_key
+        result = self.connection.execute(ins)
+        return result.inserted_primary_key[0]
 
-    def select(self, table_name : str) -> List[Tuple]:
+    def select(self, table_name: str) -> List[Tuple]:
         """Выборка данных из таблицы table_name"""
 
-        #Фильтрация данных
+        # Фильтрация данных
         if table_name not in self.tables_dict:
             raise ValueError(f"Переданной таблицы {table_name} не существует!")
 
-        #Текущая таблица
+        # Текущая таблица
         current_table = self.tables_dict[table_name]["table"]
-        
-        #Команда для выбора
+
+        # Команда для выбора
         s = select([current_table])
 
-        #Применяем команду для соединения
-        connection = self.engine.connect()
-        rp = connection.execute(s)
+        # Применяем команду для соединения
+        rp = self.connection.execute(s)
         return rp.fetchall()
+
 
 def main():
 
     # Вставка данных
 
     database_processing = DatabaseProcessing("sqlite:///Demenchuk_bank.db")
-    values = [4,"Деменчук", "Георгий","Максимович","Улица", "Город","Штат","96456","795496546","demka@mail.ru"]
-    database_processing.insert("customers",values)
+    generator = Generator()
+    for i in range(100):
+        values = generator.gen_customer()
+        pk = database_processing.insert("customers", values)
+        print(f"Записали данные с первичным ключом {pk}")
     result = database_processing.select("customers")
     print(result)
+
 
 if __name__ == "__main__":
     main()
