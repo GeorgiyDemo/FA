7139242c88496a604dbd0d9df7c4b7c9eaac7c0e
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-03-14 15:05:45 +0300

    Python & SQL праткика 5 - 10 заданий есть #116

--- Course II/Python_SQL/pract/pract5/main.py
+++ Course II/Python_SQL/pract/pract5/main.py
@@ -3,7 +3,7 @@
 Подгруппа 3
 """
 
-from sqlalchemy import select, create_engine, MetaData, Table, cast, String
+from sqlalchemy import select, create_engine, MetaData, Table, cast, String, Numeric
 from sqlalchemy.sql import expression, functions, func
 from sqlalchemy import types
 
@@ -43,8 +43,7 @@ def task_1():
 
     rp = connection.execute(s)
     results = rp.fetchall()
-    new = [item[0].upper() for item in results if item[0] is not None]
-    return new
+    return [item[0].upper() for item in results if item[0] is not None]
 
 def task_2():
     """Задание 2"""
@@ -56,17 +55,101 @@ def task_2():
         ])
 
     rp = connection.execute(s)
-    results = rp.fetchall()
-    return results
+    return rp.fetchall()
 
 def task_3():
     """Задание 3"""
-    pass
+    s = select([
+        cast(func.substr(student.c.name,1,1), String(20))+"."+
+        cast(student.c.surname, String(20))+"; место жительства-"+
+        cast(student.c.city, String(20))+"; родился:"+
+        cast(func.strftime('%d-%m-%Y',student.c.birthday),  String(20))+"."
+        ])
+
+    rp = connection.execute(s)
+    results = rp.fetchall()
+    return [item[0].lower() for item in results if item[0] is not None]
+
+def task_4():
+    """Задание 4"""
+    s = select([
+    cast(student.c.name, String(20))+" "+
+    cast(student.c.surname, String(20))+" родился в "+
+    cast(func.strftime('%Y',student.c.birthday),  String(20))+" году" +"."
+    ]).where(student.c.student_id == 10)
+
+    rp = connection.execute(s)
+    return rp.fetchone()
+
+def task_5():
+    """Задание 5"""
+    s = select([
+        student.c.name,
+        student.c.surname,
+        cast((student.c.stipend * 100), Numeric)
+    ])
+    rp = connection.execute(s)
+    return rp.fetchall()
+
+def task_6():
+    """Задание 6"""
+    s = select([
+        cast(student.c.name, String(20))+" "+
+        cast(student.c.surname, String(20))+" родился в "+
+        cast(func.strftime('%Y',student.c.birthday),  String(20))+" году" +"."
+    ]).where(student.c.kurs.in_((1,2,4)))
+    rp = connection.execute(s)
+    return rp.fetchall()
+
+def task_7():
+    """Задание 7"""
+    s = select([
+        "Код-"+cast(university.c.univ_id, String(20))+"; "+
+        cast(university.c.univ_name, String(20))+"-г."+
+        cast(university.c.city, String(20)) +"; Рейтинг="+
+        cast(university.c.rating, String(20)) +"."
+    ])
+    rp = connection.execute(s)
+    return rp.fetchall()
+
+def task_8():
+    """Задание 8"""
+    s = select([
+        "Код-"+cast(university.c.univ_id, String(20))+"; "+
+        cast(university.c.univ_name, String(20))+"-г."+
+        cast(university.c.city, String(20)) +"; Рейтинг="+
+        cast(func.round(university.c.rating, -2), String(20)) +"."
+    ])
+    rp = connection.execute(s)
+    return rp.fetchall()
+
+def task_9():
+    """Задание 9"""
+    s = select([
+        func.count(exam_marks.c.student_id)
+        ]).where(exam_marks.c.subj_id == 20)
+    rp = connection.execute(s)
+    return rp.fetchall()
+
+
+def task_10():
+    """Задание 10"""
+    s = select([
+        func.count(func.distinct(exam_marks.c.subj_id))
+        ])
+    rp = connection.execute(s)
+    return rp.fetchall()
+
+
 
 def main():
-    print(task_1())
-    print(task_2())
-    print(task_3())
+
+    tasks_list = [task_1,task_2,task_3, task_4, task_5, task_6, task_7, task_8, task_9, task_10]
+    for i in range(len(tasks_list)):
+        print(f"\nЗадание №{i+1}")
+        r = tasks_list[i]()
+        print(r)
+
 
 if __name__ == "__main__":
     main()
