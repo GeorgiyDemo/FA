396f723e83b77de8bd45eb96c183662673650490
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-04-13 00:03:21 +0300

    Перенос всех запросов, которые надо сделать #132

--- Course II/СУБД/Control/SQL_exmples.sql
+++ Course II/СУБД/Control/SQL_exmples.sql
@@ -1,58 +1,77 @@
-/*
-Задача 1
-Отображаем все продукты и их цену для определенного заказа
-*/
-SELECT
-	products.title Продукт,
-    products.price Цена,
-    products_count.count "Кол-во продуктов",
-    orders.cost "Общая стоиммость заказа"
-    
+/* 
+Задача 1 
+Отображаем все продукты и их цену для определенного заказа 
+*/ 
+
+SELECT 
+products.title Продукт, 
+products.price Цена, 
+products_count.count "Кол-во продуктов", 
+orders.cost "Общая стоиммость заказа" 
 FROM products_count
+INNER JOIN products ON products_count.product_id=products.id 
+INNER JOIN orders ON products_count.order_id=orders.id WHERE order_id=1; 
 
-INNER JOIN products ON products_count.product_id=products.id
-INNER JOIN orders ON products_count.order_id=orders.id WHERE order_id=1;
+/* 
+Задача 2 
+Отображаем клиентов, которые не делали заказ в ресторане 
+*/ 
 
+SELECT clients.* FROM clients 
+LEFT JOIN orders ON orders.client_id = clients.id 
+WHERE orders.client_id IS NULL 
+//Переделать через минус 
+
+//Добавить выборку по годам 
+
+/* 
+Задача 3 
+Получаем название домов и их стоимость за ночь по определенному бронированию 
+*/ 
+
+SELECT
+houses.name "Название дома",
+houses.price "Стоимость дома",
+bookings.cost "Общая стоимость заказа",
+bookings.date_out-bookings.date_in "Кол-во дней"
+FROM bookings
+INNER JOIN houses
+ON houses.booking_id = bookings.id WHERE bookings.id=53;
+ 
 /*
-Задача 2
-Отображаем клиентов, которые не делали заказ в ресторане
+Задача 4 
+Получение тех домов, которые простаивают в текущий момент 
 */
-SELECT clients.* FROM clients
-LEFT JOIN orders ON orders.client_id = clients.id
-WHERE orders.client_id IS NULL
 
-
-/*
-Задача 3
-TODO: Получение тех домов, которые простаивают
-*/
 SELECT * FROM HOUSES WHERE BOOKING_ID is NULL
 
 /*
-Задача 4
-TODO: Получение домов, которые когда-либо заказывал опеределенный клиент
-*/
-
-/*
 Задача 5
-Сколько всего денег определенный клиент потратил на номера и ресторан (по имени)
+Сколько всего денег определенный клиент потратил на номера и ресторан за определенный промежуток времени (ДИАПАЗОН ДАТ)
+Вывести всех за период и сортировка по кол-ву потраченных денег
 */
 
 /*
 Задача 6
-Получаем название домов и их стоимость за ночь по определенному бронированию
+Получение дохода отеля (с ресторана и бронирований) за определенный промежуток времени (ДИАПАЗОН ДАТ)
 */
-SELECT
-	houses.name "Название дома",
-    houses.price "Стоимость дома",
-    bookings.cost "Общая стоимость заказа",
-    bookings.date_out-bookings.date_in "Кол-во дней"
-FROM bookings
-INNER JOIN houses
-	ON houses.booking_id = bookings.id WHERE bookings.id=53;
 
 /*
 Задача 7
+Получение ФИО работников, которые обслуживали бронирования клиента
+*/
+
+/*
+Задача 8
+Подсчет по месяцам кол-ва бронирований за период (2019-2021)
+*/
+
+/*
+Задача 9
+Подсчет по месяцам кол-ва заказов в ресторане за период (2019-2021)
+*/
+
+/*
 Удаляем все данные из СУБД
 */
 SET SQL_SAFE_UPDATES = 0;
