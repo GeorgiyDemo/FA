b79a0e4b8af9a07a86ca76502027b10cfdebd0a5
Demka <16972045+GeorgiyDemo@users.noreply.github.com>
2022-02-17 13:48:05 +0300

    Мобильная разработка - работа на паре

---
+++ Course_III/MOBILE/task3/app/src/main/java/com/demka/clicker/MainActivity.java
@@ -0,0 +1,68 @@
+package com.demka.clicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mainText;
+    Button incButton;
+    Button decButton;
+    Button resetButton;
+    ImageView imageView;
+
+    private int score = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        incButton = findViewById(R.id.incButton);
+        decButton = findViewById(R.id.decButton);
+        resetButton = findViewById(R.id.resetButton);
+
+        mainText = findViewById(R.id.mainText);
+        imageView = findViewById(R.id.imageView3);
+
+        View.OnClickListener incListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score++;
+                String itemPlural = getResources().getQuantityString(R.plurals.plurals, score);
+                String s = "Кнопка нажата " + score + " " + itemPlural;
+                mainText.setText(s.toCharArray(), 0, s.length());
+            }
+        };
+
+        View.OnClickListener decListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score--;
+                String itemPlural = getResources().getQuantityString(R.plurals.plurals, score);
+                String s = "Кнопка нажата " + score + " " + itemPlural;
+                mainText.setText(s.toCharArray(), 0, s.length());
+            }
+        };
+
+        View.OnClickListener resetListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                score = 0;
+                String s = "Кнопка нажата 0 раз";
+                mainText.setText(s.toCharArray(), 0, s.length());
+            }
+        };
+
+        incButton.setOnClickListener(incListener);
+        imageView.setOnClickListener(incListener);
+        decButton.setOnClickListener(decListener);
+        resetButton.setOnClickListener(resetListener);
+    }
+}
