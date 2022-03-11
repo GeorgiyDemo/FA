b7459ac15e969f072a15812b33d4b6cc0a608dca
Demka <16972045+GeorgiyDemo@users.noreply.github.com>
2022-03-11 16:40:19 +0300

    Haskell - t-5 вроде норм #225

--- Course_III/HASKELL/lab/lab3/t-5-2.hs
+++ Course_III/HASKELL/lab/lab3/t-5-2.hs
@@ -1,52 +1,62 @@
-data Area = Union [Area] | 
-           Inters [Area] | 
-           Empty         | 
-           Rectangle Int Int Int Int |
-           Circle Int Int Int deriving (Eq,Show)
- 
--- Вспомогательная функция, возвращающая
--- пересечение двух отрезков в виде списка [a,b]
--- Если отрезки не пересекаются - возвращает пустой список
- 
-int2 :: [Int] -> [Int] -> [Int]
-int2 [x11,x12] [x21,x22] | (x21 < x11) = int2 [x21,x22] [x11,x12]
-int2 [x11,x12] [x21,x22] | (x12 < x21)  = []
-                         | (x21 >= x11) && (x21 <= x12) && (x22 >= x11) && (x22 <= x12) = [x21,x22]
-                         | otherwise = [x21,x12]
-  
--- Набросок (не окончен) - пересечение двух областей
--- не хватает пересечения окружности и прямоугольника
--- и полной реализации пересечения объединений и пересечений
- 
-intersect :: Area -> Area -> Area
-intersect Empty _ = Empty
-intersect _ Empty = Empty
-intersect (Rectangle x11 y11 x12 y12)
-          (Rectangle x21 y21 x22 y22) = if (xx==[]) || (yy==[]) then 
-                                           Empty 
-                                        else 
-                                           (Rectangle (xx!!0) (yy!!0) (xx!!1) (yy!!1))
-         where xx = int2 [x11,x12] [x21,x22]
-               yy = int2 [y12,y11] [y22,y21]
-intersect (Circle x1 y1 r1)
-          (Circle x2 y2 r2) = if (x1-x2)^2+(y1-y2)^2 <= (r1+r2)^2 then
-                                 Inters [(Circle x1 y1 r1),(Circle x2 y2 r2)]
-                              else
-                                 Empty                              
-intersect (Union ars) (Rectangle x1 y1 x2 y2) = (Union (map (\ a -> intersect a (Rectangle x1 y1 x2 y2)) ars))             
--- и т.п.          
-                           
-isEmpty :: Area -> Bool
-isEmpty Empty               = True
-isEmpty (Circle _ _ _)      = False
-isEmpty (Rectangle _ _ _ _) = False
-isEmpty (Union ars)         = all (/= Empty) ars
-isEmpty (Inters ars)        = (foldl (\ acc fi -> intersect acc fi) (head ars) (tail ars))==Empty           
- 
- 
-isRectangular :: Area -> Bool
-isRectangular Empty  = False
-isRectangular (Rectangle _ _ _ _) = True
-isRectangular (Circle _ _ _)      = False
-isRectangular (Union ars)         = all (isRectangular) ars
-isRectangular (Inters ars)        = all (isRectangular) ars
+{-
+ПИ19-3 Деменчук
+Задание 8.2 (по выбору)
+
+8.	Иерархия должностей в некоторой организации образует древовидную структуру.
+Каждый работник, однозначно характеризующийся уникальным именем, имеет несколько подчиненных.
+Определите тип данных, представляющий такую иерархию и опишите следующие функции:
+
+2)	getAllSubordinate, возвращающую список всех подчиненных данного работника, включая косвенных.
+-}
+
+-- Создаем кастомный тип, согласно заданию
+-- у каждого работника есть массив подчиненных
+data Employee = JobWorker (String) [Employee] deriving (Show, Eq)
+
+-- Метод по поиску значения
+find ((employee, emp) :xs) manager =
+    if employee == manager then emp
+    else find xs manager
+
+-- Список имён работников
+workerNamesList (JobWorker (n) emp) = n:concatMap workerNamesList emp
+
+-- Список всех работников
+employeeList (JobWorker (name) emp) = (name, emp):concatMap employeeList emp
+
+-- getAllSubordinate, возвращает список всех подчиненных  работника
+getAllSubordinate employee (JobWorker (name) emp) = 
+    concat (map (\item -> workerNamesList item) (find (employeeList
+    (JobWorker (name) emp)) employee))
+
+-- Точка входа в приложение
+main = do
+
+    -- Формируем тестовую иерархию работников 1
+    print(getAllSubordinate "Ivanov" (
+        JobWorker "Ivanov"[
+            JobWorker "Petrov"
+                [
+                    JobWorker "Sidorov" [],
+                    JobWorker "Demko" [],
+                    JobWorker "Dmitriev" []
+                ],
+            JobWorker "Maximov" []
+        ]
+        ))
+
+    -- Представляем иерархию работников в виде чисел для наглядности
+    print(getAllSubordinate "Petrov" (
+        JobWorker "Petrov" [JobWorker "1"
+                            [JobWorker "1.1" [],
+                            JobWorker "1.2" [],
+                            JobWorker "1.3" []
+                            ],
+                        JobWorker "2"
+                            [JobWorker "2.1" [
+                                JobWorker "2.1.1" []
+                            ],
+                            JobWorker "2.2" [],
+                            JobWorker "2.3" []
+                            ]
+                        ]))
