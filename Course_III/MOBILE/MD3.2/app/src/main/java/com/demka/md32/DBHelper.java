e125cfadca4582484b5ba99f0e7a52bccafc5254
Demka <16972045+GeorgiyDemo@users.noreply.github.com>
2022-03-18 18:22:18 +0300

    Более нормальная структура для работы с БД

--- Course_III/MOBILE/MD3.2/app/src/main/java/com/demka/md32/DBHelper.java
+++ Course_III/MOBILE/MD3.2/app/src/main/java/com/demka/md32/DBHelper.java
@@ -1,24 +1,67 @@
 package com.demka.md32;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBHelper extends SQLiteOpenHelper {
+
     public DBHelper(Context context) {
         super(context, "MyDB", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        // создаем таблицу с полями
-        db.execSQL("create table mytable ("
-                + "id integer primary key autoincrement,"
-                + "name text" + ");");
+        db.execSQL("CREATE TABLE IF NOT EXISTS mytable (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    public void addData(String data) {
+
+        ContentValues cv = new ContentValues();
+        SQLiteDatabase db = getWritableDatabase();
+
+        Log.i("LOG_TAG", data);
+        cv.put("name", data);
+        db.insert("mytable", null, cv);
+    }
+
+    public boolean deleteData(String data) {
+        ContentValues cv = new ContentValues();
+        SQLiteDatabase db = getWritableDatabase();
+        String whereClause = String.format("name='%s'", data);
+        int result = db.delete("mytable", whereClause, null);
+        return result == 1;
+    }
+
+    public List<String> getData() {
+        List<String> resultList = new ArrayList<>();
+        SQLiteDatabase db = getWritableDatabase();
+
+        Cursor c = db.query("mytable", null, null, null, null, null, null);
+        if (c.moveToFirst()) {
+            int idColIndex = c.getColumnIndex("id");
+            int nameColIndex = c.getColumnIndex("name");
+            do {
+
+                resultList.add(c.getString(nameColIndex));
+                Log.d("LOG_TAG", "ID = " + c.getInt(idColIndex) + ", name = " + c.getString(nameColIndex));
+            } while (c.moveToNext());
+
+        } else
+            Log.d("LOG_TAG", "0 rows");
+        c.close();
+
+        return resultList;
     }
 }
--- Course_III/MOBILE/MD3.2/app/src/main/java/com/demka/md32/MainActivity.java
+++ Course_III/MOBILE/MD3.2/app/src/main/java/com/demka/md32/MainActivity.java
@@ -1,10 +1,11 @@
 package com.demka.md32;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +20,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnAdd = findViewById(R.id.btnAdd);
+        btnDelete = findViewById(R.id.btnDelete);
+        btnRead = findViewById(R.id.btnRead);
+        etName = findViewById(R.id.etName);
 
+        View.OnClickListener addButtonListener = this::addButtonClicked;
+        View.OnClickListener deleteButtonListener = this::deleteButtonClicked;
+        View.OnClickListener readButtonListener = this::readButtonClicked;
+
+        btnAdd.setOnClickListener(addButtonListener);
+        btnDelete.setOnClickListener(deleteButtonListener);
+        btnRead.setOnClickListener(readButtonListener);
+
+        dbHelper = new DBHelper(this);
+
+    }
+
+    public void addButtonClicked(View v) {
+        String data = etName.getText().toString();
+        dbHelper.addData(data);
+    }
+
+    public void deleteButtonClicked(View v) {
+        String data = etName.getText().toString();
+        dbHelper.deleteData(data);
+
+    }
+
+    public void readButtonClicked(View v) {
+        dbHelper.getData();
     }
 }
