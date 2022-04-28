1fc9b35a81031e33831692c5f331c4a5f6135a39
Demka <16972045+GeorgiyDemo@users.noreply.github.com>
2022-04-28 12:26:47 +0300

    Haskell t-8 #254

--- Course_III/HASKELL/lab/lab6/t-8.hs
+++ Course_III/HASKELL/lab/lab6/t-8.hs
@@ -3,3 +3,21 @@
 Определите следующие функции с использованием функций высшего порядка:
 - Переставить в матрице две строки.
 -}
+
+-- Метод для перемещения элементов списка по индексам
+-- Был реализован в задании t-10
+swapTwo :: Int -> Int -> [a] -> [a]
+swapTwo f s xs = map snd . foldr (\x a -> 
+        if fst x == f then ys !! s : a
+        else if fst x == s then ys !! f : a
+        else x : a) [] $ ys
+    where ys = zip [0..] xs
+
+-- Точка входа в приложение
+main :: IO ()
+main = do
+
+    let textMatrix1 = [[1,2,3], [4,5,6], [7,8,9]]
+    print("Matrix: " ++ show(textMatrix1))
+    print("swapTwo 0 2: " ++ show(swapTwo 0 2 textMatrix1))
+    print("swapTwo 0 1: " ++ show(swapTwo 0 1 textMatrix1))
