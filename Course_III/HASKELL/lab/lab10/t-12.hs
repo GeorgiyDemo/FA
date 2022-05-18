34f01c35885ac083f0a15651c3220eae1c478edc
Demka <16972045+GeorgiyDemo@users.noreply.github.com>
2022-05-18 15:20:08 +0300

    Haskell t-12 #261

--- Course_III/HASKELL/lab/lab10/t-12.hs
+++ Course_III/HASKELL/lab/lab10/t-12.hs
@@ -1,9 +1,7 @@
--- Вариант b
-
+-- Вариант 6
 {-
 Построить КА, допускающий цепочки из {0,1}* 
 в них есть подцепочка 11 и подцепочка 00 
-
 -}
 
 -- Проверка на единицу или 0, если не 1 и не 0 -> False
@@ -11,38 +9,25 @@ isZeroOrOne :: Char -> Bool
 isZeroOrOne x = x == '0' || x == '1'
 
 
-isSubStringFound :: String -> String -> Bool
-isSubStringFound [] _ = True
-isSubStringFound _ [] = False
-isSubStringFound (x:xs) (y:ys)
-  | x == y = isSubStringFound xs ys
-  | otherwise = isSubStringFound (x:xs) ys
-  
-zeroOneChain19 :: String -> Bool
-zeroOneChain19 s = let
-
-  match2 ('0':'0':xs) = find00 xs "00"
-  match2 _ = False
-
-
-  find00 _ [] = True
-  find00 (x:xs) (p:ps)
-    | x == p = find00 xs ps
-    | otherwise = find00 xs "00" 
-  find00 _ _ = False
-
+myChain :: String -> Bool
+myChain s = let
+  check m = let
+      check' [] _ = True
+      check' (y:ys) (x:xs)
+        | x == y = check' ys xs
+        | otherwise = check' m xs
+      check' _ [] = False
+    in check' m
+   
   in
-    match2 s && all isZeroOrOne s
-
+    -- Проверка на наличие подцепочки 11 и 00 и что элементы 0 и 1 в цепочке
+    check "11" s && check "00" s && all isZeroOrOne s  
 
 main = do
-    print(zeroOneChain19 "00")
-    print(zeroOneChain19 "11")
-    print(zeroOneChain19 "0011")
-    print(zeroOneChain19 "1100")
-    print(zeroOneChain19 "001100")
-    print(zeroOneChain19 "110011")
-    print("---")
-    print(isSubStringFound "11" "122")
-    print(isSubStringFound "11" "123")
-    print(isSubStringFound "55" "12341556")
+    print(myChain "00")
+    print(myChain "11")
+    print(myChain "0011")
+    print(myChain "1100")
+    print(myChain "001100")
+    print(myChain "110011")
+    print(myChain "1210011")
